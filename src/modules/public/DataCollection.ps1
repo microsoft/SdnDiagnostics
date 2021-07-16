f06e0679f44ee12b6e90f29ae6d4d319f630329d
Adam Rudell <arudell@microsoft.com>
2021-07-16 15:10:44 -0500

    architecture changes

---
+++ src/modules/public/DataCollection.ps1
@@ -0,0 +1,321 @@
+function Get-SdnSlbMuxConfigurationState {
+    <#
+    #>
+
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory = $true)]
+        [System.IO.FileInfo]$OutputDirectory
+    )
+
+    $ProgressPreference = 'SilentlyContinue'
+
+    try {
+        $config = Get-SdnRoleConfiguration -Role:SoftwareLoadBalancer
+
+        # ensure that the appropriate windows feature is installed and ensure module is imported
+        $confirmFeatures = Confirm-RequiredFeaturesInstalled -Name $config.windowsFeature
+        if(!$confirmFeatures){
+            throw New-Object System.Exception("Required feature is missing")
+        }
+
+        $confirmModules = Confirm-RequiredModulesLoaded -Name $config.requiredModules
+        if(!$confirmModules){
+            throw New-Object System.Exception("Required module is not loaded")
+        }
+
+        # create the OutputDirectory if does not already exist
+        if(!(Test-Path -Path $OutputDirectory.FullName -PathType Container)){
+            $null = New-Item -Path $OutputDirectory.FullName -ItemType Directory -Force
+        }
+
+        # dump out the regkey properties
+        Export-RegistryKeyConfigDetails -Path $config.properties.regKeyPaths -OutputDirectory (Join-Path -Path $OutputDirectory.FullName -ChildPath "Registry")
+
+        # output slb configuration and states
+        "Getting MUX Driver Control configuration settings" | Trace-Output -Level:Verbose
+        MuxDriverControlConsole.exe /GetMuxState | Export-ObjectToFile -FilePath $OutputDirectory.FullName -Name 'GetMuxState' -FileType txt
+        MuxDriverControlConsole.exe /GetMuxConfig | Export-ObjectToFile -FilePath $OutputDirectory.FullName -Name 'GetMuxConfig' -FileType txt
+        MuxDriverControlConsole.exe /GetMuxStats | Export-ObjectToFile -FilePath $OutputDirectory.FullName -Name 'GetMuxStats' -FileType txt
+        MuxDriverControlConsole.exe /GetMuxVipList | Export-ObjectToFile -FilePath $OutputDirectory.FullName -Name 'GetMuxVipList' -FileType txt
+        MuxDriverControlConsole.exe /GetMuxDripList | Export-ObjectToFile -FilePath $OutputDirectory.FullName -Name 'GetMuxDripList' -FileType txt
+        MuxDriverControlConsole.exe /GetStatelessVip | Export-ObjectToFile -FilePath $OutputDirectory.FullName -Name 'GetStatelessVip' -FileType txt
+        MuxDriverControlConsole.exe /GetStatefulVip | Export-ObjectToFile -FilePath $OutputDirectory.FullName -Name 'GetStatefulVip' -FileType txt 
+
+        Get-GeneralConfigurationState -OutputDirectory $OutputDirectory.FullName
+    }
+    catch {
+        "{0}`n{1}" -f $_.Exception, $_.ScriptStackTrace | Trace-Output -Level:Error
+    }
+
+    $ProgressPreference = 'Continue'
+}
+
+function Get-SdnServerConfigurationState {
+    <#
+    #>
+
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory = $true)]
+        [System.IO.FileInfo]$OutputDirectory
+    )
+
+    $ProgressPreference = 'SilentlyContinue'
+
+    try {
+        $config = Get-SdnRoleConfiguration -Role:Server
+
+        # ensure that the appropriate windows feature is installed and ensure module is imported
+        $confirmFeatures = Confirm-RequiredFeaturesInstalled -Name $config.windowsFeature
+        if(!$confirmFeatures){
+            throw New-Object System.Exception("Required feature is missing")
+        }
+
+        $confirmModules = Confirm-RequiredModulesLoaded -Name $config.requiredModules
+        if(!$confirmModules){
+            throw New-Object System.Exception("Required module is not loaded")
+        }
+
+        # create the OutputDirectory if does not already exist
+        if(!(Test-Path -Path $OutputDirectory.FullName -PathType Container)){
+            $null = New-Item -Path $OutputDirectory.FullName -ItemType Directory -Force
+        }
+
+        # dump out the regkey properties
+        Export-RegistryKeyConfigDetails -Path $config.properties.regKeyPaths -OutputDirectory (Join-Path -Path $OutputDirectory.FullName -ChildPath "Registry")
+
+        # Gather VFP port configuration details
+        "Gathering VFP port details" | Trace-Output -Level:Verbose
+        foreach ($vm in (Get-WmiObject -na root\virtualization\v2 msvm_computersystem)){
+            foreach ($vma in $vm.GetRelated("Msvm_SyntheticEthernetPort")){
+                foreach ($port in $vma.GetRelated("Msvm_SyntheticEthernetPortSettingData").GetRelated("Msvm_EthernetPortAllocationSettingData").GetRelated("Msvm_EthernetSwitchPort")){
+                    
+                    $outputDir = New-Item -Path (Join-Path -Path $OutputDirectory.FullName -ChildPath "VFP\$($vm.ElementName)") -ItemType Directory -Force
+                    vfpctrl /list-nat-range /port $($port.Name) | Export-ObjectToFile -FilePath $outputDir.FullName -Prefix 'NatInfo' -Name $port.Name -FileType txt
+                    vfpctrl /list-rule /port $($port.Name) | Export-ObjectToFile -FilePath $outputDir.FullName -Prefix 'RuleInfo' -Name $port.Name -FileType txt
+                    vfpctrl /list-mapping /port $($port.Name) | Export-ObjectToFile -FilePath $outputDir.FullName -Prefix 'ListMapping' -Name $port.Name -FileType txt
+                }
+            }
+        }
+
+        vfpctrl /list-vmswitch-port | Export-ObjectToFile -FilePath $OutputDirectory.FullName -Name 'vfpctrl_list-vmswitch-port' -FileType json
+
+        # Gather OVSDB databases
+        "Gathering ovsdb database output" | Trace-Output -Level:Verbose
+        ovsdb-client.exe dump tcp:127.0.0.1:6641 ms_vtep | Export-ObjectToFile -FilePath $OutputDirectory.FullName -Name 'ovsdb_vtep' -FileType txt
+        ovsdb-client.exe dump tcp:127.0.0.1:6641 ms_firewall | Export-ObjectToFile -FilePath $OutputDirectory.FullName -Name 'ovsdb_firewall' -FileType txt
+
+        # Gather Hyper-V network details
+        "Gathering hyper-v configuration details" | Trace-Output -Level:Verbose
+        Get-PACAMapping | Sort-Object PSComputerName | Export-ObjectToFile -FilePath $OutputDirectory.FullName -Name 'Get-PACAMapping' -FileType txt -Format Table
+        Get-ProviderAddress | Sort-Object PSComputerName | Export-ObjectToFile -FilePath $OutputDirectory.FullName -Name 'Get-ProviderAddress' -FileType txt -Format Table
+        Get-CustomerRoute | Sort-Object PSComputerName | Export-ObjectToFile -FilePath $OutputDirectory.FullName -Name 'Get-CustomerRoute' -FileType txt -Format Table
+        Get-NetAdapterVPort | Export-ObjectToFile -FilePath $OutputDirectory.FullName -Name 'Get-NetAdapterVPort' -FileType txt -Format Table
+        Get-NetAdapterVmqQueue | Export-ObjectToFile -FilePath $OutputDirectory.FullName -Name 'Get-NetAdapterVmqQueue' -FileType txt -Format Table
+        Get-VMSwitch | Export-ObjectToFile -FilePath $OutputDirectory.FullName -Name 'Get-VMSwitch' -FileType txt -Format List 
+        Get-VMSwitchTeam | Export-ObjectToFile -FilePath $OutputDirectory.FullName -Name 'Get-VMSwitchTeam' -FileType txt -Format List
+        Get-VMNetworkAdapterPortProfile -AllVMs | Export-ObjectToFile -FilePath $OutputDirectory.FullName -Name 'Get-VMNetworkAdapterPortProfile' -FileType txt -Format Table
+
+        Get-GeneralConfigurationState -OutputDirectory $OutputDirectory.FullName
+    }
+    catch {
+        "{0}`n{1}" -f $_.Exception, $_.ScriptStackTrace | Trace-Output -Level:Error
+    }
+
+    $ProgressPreference = 'Continue'
+}
+
+function Get-SdnNetControllerConfigurationState {
+    <#
+    #>
+
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory = $true)]
+        [System.IO.FileInfo]$OutputDirectory
+    )
+
+    $ProgressPreference = 'SilentlyContinue'
+    
+    try {
+        $config = Get-SdnRoleConfiguration -Role:NetworkController
+
+        # ensure that the appropriate windows feature is installed and ensure module is imported
+        $confirmFeatures = Confirm-RequiredFeaturesInstalled -Name $config.windowsFeature
+        if(!$confirmFeatures){
+            throw New-Object System.Exception("Required feature is missing")
+        }
+
+        $confirmModules = Confirm-RequiredModulesLoaded -Name $config.requiredModules
+        if(!$confirmModules){
+            throw New-Object System.Exception("Required module is not loaded")
+        }
+
+        # create the OutputDirectory if does not already exist
+        if(!(Test-Path -Path $OutputDirectory.FullName -PathType Container)){
+            $null = New-Item -Path $OutputDirectory.FullName -ItemType Directory -Force
+        }
+
+        # dump out the regkey properties
+        Export-RegistryKeyConfigDetails -Path $config.properties.regKeyPaths -OutputDirectory (Join-Path -Path $OutputDirectory.FullName -ChildPath "Registry")
+
+        # enumerate dll binary version for NC application
+        "Getting"
+        $ncAppDirectories = Get-ChildItem -Path "C:\Windows\NetworkController" -Directory
+        $outputDir = New-Item -Path (Join-Path -Path $OutputDirectory.FullName -ChildPath "NCApplication") -ItemType Directory -Force
+        foreach($directory in $ncAppDirectories){
+            [System.String]$fileName = "FileInfo_{0}" -f $directory.BaseName
+            Get-Item -Path "$($directory.FullName)\*" -Include *.dll,*.exe | Export-ObjectToFile -FilePath $outputDir.FullName -Name $fileName -FileType txt -Format List
+        }
+
+        Get-GeneralConfigurationState -OutputDirectory $OutputDirectory.FullName
+    }
+    catch {
+        "{0}`n{1}" -f $_.Exception, $_.ScriptStackTrace | Trace-Output -Level:Error
+    }
+
+    $ProgressPreference = 'Continue'
+}
+
+function Get-SdnGatewayConfigurationState {
+    <#
+    #>
+
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory = $true)]
+        [System.IO.FileInfo]$OutputDirectory
+    )
+
+    $ProgressPreference = 'SilentlyContinue'
+
+    try {
+        $config = Get-SdnRoleConfiguration -Role:Gateway
+
+        # ensure that the appropriate windows feature is installed and ensure module is imported
+        $confirmFeatures = Confirm-RequiredFeaturesInstalled -Name $config.windowsFeature
+        if(!$confirmFeatures){
+            throw New-Object System.Exception("Required feature is missing")
+        }
+
+        $confirmModules = Confirm-RequiredModulesLoaded -Name $config.requiredModules
+        if(!$confirmModules){
+            throw New-Object System.Exception("Required module is not loaded")
+        }
+
+        # create the OutputDirectory if does not already exist
+        if(!(Test-Path -Path $OutputDirectory.FullName -PathType Container)){
+            $null = New-Item -Path $OutputDirectory.FullName -ItemType Directory -Force
+        }
+
+        # dump out the regkey properties
+        Export-RegistryKeyConfigDetails -Path $config.properties.regKeyPaths -OutputDirectory (Join-Path -Path $OutputDirectory.FullName -ChildPath "Registry")
+
+        # dump out the role configuration state properties
+        "Getting RRAS VPN configuration details" | Trace-Output -Level:Verbose
+        Get-VpnServerConfiguration | Export-ObjectToFile -FilePath $OutputDirectory.FullName -Name 'Get-VpnServerConfiguration' -FileType txt -Format Table
+        Get-VpnS2SInterface | Export-ObjectToFile -FilePath $OutputDirectory.FullName -Name 'Get-VpnS2SInterface' -FileType txt -Format List
+        Get-RemoteaccessRoutingDomain | Export-ObjectToFile -FilePath $OutputDirectory.FullName -Name 'Get-RemoteaccessRoutingDomain' -FileType txt -Format List
+
+        foreach ($routingDomain in Get-RemoteAccessRoutingDomain){
+            "Getting properties for routing domain {0}" -f $routingDomain.RoutingDomain | Trace-Output -Level:Verbose
+            $routingDomainPath = New-Item -Path (Join-Path -Path $OutputDirectory.FullName -ChildPath $routingDomain.RoutingDomainID) -ItemType Directory -Force
+            Get-BgpRouter -RoutingDomain $routingDomain.RoutingDomain | Export-ObjectToFile -FilePath $routingDomainPath.FullName -Name 'Get-BgpRouter' -FileType txt -Format List
+            Get-BgpRouter -RoutingDomain $routingDomain.RoutingDomain | Get-BgpPeer | Export-ObjectToFile -FilePath $routingDomainPath.FullName -Name 'Get-BgpPeer' -FileType txt -Format List
+            Get-BgprouteInformation -RoutingDomain $routingDomain.RoutingDomain | Export-ObjectToFile -FilePath $routingDomainPath.FullName -Name 'Get-BgprouteInformation' -FileType txt -Format List
+            Get-BgpCustomRoute -RoutingDomain $routingDomain.RoutingDomain | Export-ObjectToFile -FilePath $routingDomainPath.FullName -Name 'Get-BgpCustomRoute' -FileType txt -Format List
+        }
+
+        # for ipsec fast path, there is a new service w/ new cmdlets to get the tunnels and routing domains
+        if((Get-Service -Name GatewayService).Status -ieq 'Running'){
+            "GatewayService is enabled. Getting GatewayService configuration details" | Trace-Output -Level:Verbose
+            $gatewayServicePath = New-Item -Path (Join-Path -Path $OutputDirectory.FullName -ChildPath 'GatewayService') -ItemType Directory -Force
+            Get-Service -Name GatewayService | Export-ObjectToFile -FilePath $gatewayServicePath.FullName -Prefix 'GatewayService' -Name 'Get-Service' -FileType txt -Format List
+            Get-GatewayConfiguration | Export-ObjectToFile -FilePath $gatewayServicePath.FullName -Name 'Get-GatewayConfiguration' -FileType txt -Format List
+            Get-GatewayRoutingDomain | Export-ObjectToFile -FilePath $gatewayServicePath.FullName -Name 'Get-GatewayRoutingDomain' -FileType txt -Format List
+            Get-GatewayTunnel | Export-ObjectToFile -FilePath $gatewayServicePath.FullName -Name 'Get-GatewayTunnel' -FileType txt -Format List
+            Get-GatewayTunnelStatistics | Export-ObjectToFile -FilePath $gatewayServicePath.FullName -Name 'Get-GatewayTunnelStatistics' -FileType txt -Format List
+
+            foreach($routingDomain in  Get-GatewayRoutingDomain){
+                "Getting properties for routing domain {0}" -f $routingDomain.RoutingDomain | Trace-Output -Level:Verbose
+                $routingDomainPath = New-Item -Path (Join-Path -Path $gatewayServicePath.FullName -ChildPath $routingDomain.RoutingDomain) -ItemType Directory -Force
+                Get-BgpRouter -RoutingDomain $routingDomain.RoutingDomain | Export-ObjectToFile -FilePath $routingDomainPath.FullName -Name 'Get-BgpRouter' -FileType txt -Format List
+                Get-BgpRouter -RoutingDomain $routingDomain.RoutingDomain | Get-BgpPeer | Export-ObjectToFile -FilePath $routingDomainPath.FullName -Name 'Get-BgpPeer' -FileType txt -Format List
+                Get-BgpRouteInformation -RoutingDomain $routingDomain.RoutingDomain | Export-ObjectToFile -FilePath $routingDomainPath.FullName -Name 'Get-BgpRouteInformation' -FileType txt -Format List
+                Get-BgpCustomRoute -RoutingDomain $routingDomain.RoutingDomain | Export-ObjectToFile -FilePath $routingDomainPath.FullName -Name 'Get-BgpCustomRoute' -FileType txt -Format List
+            }
+        }
+    }
+    catch {
+        "{0}`n{1}" -f $_.Exception, $_.ScriptStackTrace | Trace-Output -Level:Error
+    }
+
+    $ProgressPreference = 'Continue'
+}
+
+function Start-SdnDataCollection {
+
+    <#
+        .SYNOPSIS
+        Automated network diagnostics and data collection/tracing script.
+
+        .PARAMETER DataCollectionType 
+        Optional parameter that allows the user to define if they want to collect either Configuration, Logs or None. Default is Logs
+            Configuration - Gathers basic configuration details related to SDN infrastructure
+            Logs - Gathers diagnostics logs and event traces, in addition to configuration details
+
+        .PARAMETER Scenario
+        Required parameter that allows you to specify the datapath scenario you want to enable
+            SLB - Adds the appropriate SLB nodes as part of the data collection 
+            Gateway - Add the appropriate Gateway nodes as part of the data collection
+            None - No scenario
+
+        .PARAMETER EnableNetworkTraces
+        Optional switch parameter that allows you to collect network traces as part of the data collection process
+
+        .PARAMETER OutputDirectory
+
+        .PARAMETER RemoteSharePath
+        Optional parameter that allows you to output results to a remote network share
+
+        .PARAMETER RemoteShareCredentials
+        Optional parameter used in conjuction with RemoteSharePath that provides appropriate credentials to access the RemoteSharePath location
+
+        .PARAMETER FromDate
+        Optional parameter that allows you to control how many hours worth of logs to retrieve from the system for the roles identified. Default is 120 hours.
+            (Get-Date).AddHours(-4)
+
+        .PARAMETER MaxTraceSize
+        Optional parameter that allows you to define maximum size allowed for network trace. Default is 2048 MB
+    #>
+
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory = $true)]
+        [Uri]$NcUri,
+
+        [Parameter(Mandatory = $false)]
+        [System.IO.FileInfo]$OutputDirectory,
+
+        [Parameter(Mandatory=$false)]
+        [ValidateSet('Configuration','Logs','None')]
+        [System.String]$DataCollectionType = 'Logs',
+
+        [Parameter(Mandatory = $false)]
+        [System.Management.Automation.PSCredential]
+        [System.Management.Automation.Credential()]
+        $Credential = [System.Management.Automation.PSCredential]::Empty
+    )
+
+    "Starting SDN Data Collection" | Trace-Output
+    if($null -eq $OutputDirectory){
+        [System.IO.FileInfo]$OutputDirectory = (Get-WorkingDirectory)
+    }
+
+    [System.IO.FileInfo]$outputDir = (Join-Path -Path $OutputDirectory.FullName -ChildPath (Get-FormattedDateTimeUTC))
+    "Results will be saved to {0}" -f $outputDir.FullName | Trace-Output
+
+    "Generating output of the NC API resources" | Trace-Output
+    Get-SdnApiResources -NcUri $NcUri.AbsoluteUri -OutputDirectory $OutputDirectory.FullName -Credential $Credential
+}
