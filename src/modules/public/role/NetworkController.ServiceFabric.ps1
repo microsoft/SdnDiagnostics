6f6f8d581af917391cde1dec747f62699d3ae538
Adam Rudell <arudell@microsoft.com>
2021-07-22 17:11:30 -0500

    add fault tolerance to sf bug

--- src/modules/public/role/NetworkController.ServiceFabric.ps1
+++ src/modules/public/role/NetworkController.ServiceFabric.ps1
@@ -34,32 +34,59 @@ function Invoke-SdnServiceFabricCommand {
         }
 
         foreach($controller in $NetworkController){
-            "Attempting to invoke Service Fabric cmdlets against {0}" -f $controller | Trace-Output -Level:Verbose
-            $session = New-PSRemotingSession -ComputerName $controller -Credential $Credential
-            $connection = Invoke-Command -Session $session -HideComputerName -ScriptBlock {
-                # The 3>$null 4>$null sends unwanted verbose and debug streams into the bit bucket
-                Connect-ServiceFabricCluster 3>$null 4>$null
-            }
 
-            if($connection){
-                "NetworkController: {0}, ScriptBlock: {1}" -f $controller, $ScriptBlock.ToString() | Trace-Output -Level:Verbose
-                $sfResults = Invoke-Command -Session $session -ScriptBlock $ScriptBlock
-                if($sfResults){
+            $i = 0
+            $maxRetry = 3
+
+            # due to scenario as described in https://docs.microsoft.com/en-us/azure/service-fabric/service-fabric-troubleshoot-local-cluster-setup#cluster-connection-fails-with-object-is-closed
+            # we want to catch any exception when connecting to service fabric cluster, and if necassary destroy and create a new remote pssession
+            "Invoke Service Fabric cmdlets against {0}" -f $controller | Trace-Output
+            while($i -lt $maxRetry){
+                $i++
+
+                $session = New-PSRemotingSession -ComputerName $controller -Credential $Credential
+                if(!$session){
+                    "No session could be established to {0}" -f $controller | Trace-Output -Level:Error
                     break
                 }
+    
+                try {
+                    $connection = Invoke-Command -Session $session -HideComputerName -ScriptBlock {
+                        # The 3>$null 4>$null sends unwanted verbose and debug streams into the bit bucket
+                        Connect-ServiceFabricCluster 3>$null 4>$null
+                    } -ErrorAction Stop
+                }
+                catch {
+                    "Unable to connect to Service Fabric Cluster. Attempt {0}/{1}`n`t{2}" -f $i, $maxRetry, $_ | Trace-Output -Level:Error
+                    "Terminating remote session {0} to {1}" -f $session.Name, $session.ComputerName | Trace-Output -Level:Warning
+                    Get-PSSession -Id $session.Id | Remove-PSSession
+                }
             }
-            else {
-                "Unable to execute ServiceFabric commands to {0}" -f $controller | Trace-Output -Level:Error
+
+            if(!$connection){
+                "Unable to connect to Service Fabric Cluster" | Trace-Output -Level:Error
+                continue
+            }
+
+            "NetworkController: {0}, ScriptBlock: {1}" -f $controller, $ScriptBlock.ToString() | Trace-Output -Level:Verbose
+            $sfResults = Invoke-Command -Session $session -ScriptBlock $ScriptBlock
+
+            # if we get results from service fabric, then we want to break out of the loop
+            if($sfResults){
+                break
             }
         }
 
-        if($sfResults) {
-            if($sfResults.GetType().IsPrimitive -or ($sfResults -is [String])) {
-                return $sfResults
-            }
+        if(!$sfResults){
+            throw New-Object System.NullReferenceException("Unable to return results from service fabric") 
         }
-                
-        return $sfResults | Select-Object -Property * -ExcludeProperty PSComputerName, RunspaceId
+        
+        if($sfResults.GetType().IsPrimitive -or ($sfResults -is [String])) {
+            return $sfResults
+        }
+        else {
+            return ($sfResults | Select-Object -Property * -ExcludeProperty PSComputerName, RunspaceId)
+        }
     }
     catch {
         "{0}`n{1}" -f $_.Exception, $_.ScriptStackTrace | Trace-Output -Level:Error
