21c9faa0306c7f30e1d531952b049dc578a23e97
Adam Rudell <arudell@microsoft.com>
2021-07-27 21:10:19 -0500

    create file copy function (#9)

--- src/modules/public/utilities.ps1
+++ src/modules/public/utilities.ps1
@@ -16,22 +16,13 @@ function Install-SdnDiagnostic {
         $modulePath = Get-Item -Path "$PSScriptRoot\..\..\..\"
         "SdnDiagnostics module found at {0}" -f $modulePath.FullName | Trace-Output -Level:Verbose
 
-        foreach($obj in $ComputerName){
-
-            # skip if for any reason the computer defined is the current machine
-            # the command is being executed from
-            if(Test-ComputerNameIsLocal -ComputerName $obj){
-                "Detected that {0} is local machine. Skipping" -f $obj | Trace-Output
-                continue
-            }
-
-            $session = New-PSRemotingSession -ComputerName $obj -Credential $Credential
-            "Copying {0} to {1} via WinRM" -f $modulePath.FullName, $session.ComputerName | Trace-Output
-            Copy-Item -Path $modulePath.FullName -Destination 'C:\Program Files\WindowsPowerShell\Modules' -Recurse -ToSession $session -Force
-
-            # ensure that we destroy the current pssessions for the computer to prevent any odd caching issues
-            Get-PSSession -ComputerName $session.ComputerName | Remove-PSSession
+        if($modulePath){
+            Copy-FileToPSRemoteSession -Path $modulePath.FullName -ComputerName $ComputerName -Destination 'C:\Program Files\WindowsPowerShell\Modules' `
+                -Credential $Credential -Recurse -Force
         }
+
+        # ensure that we destroy the current pssessions for the computer to prevent any odd caching issues
+        Get-PSSession -ComputerName $ComputerName | Remove-PSSession
     }
     catch {
         "{0}`n{1}" -f $_.Exception, $_.ScriptStackTrace | Trace-Output -Level:Error
