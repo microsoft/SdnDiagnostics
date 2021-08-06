5d4641d201a998a1188a66f166a5799e144ecc03
Adam Rudell <arudell@microsoft.com>
2021-08-05 21:09:54 -0500

    add file header

--- src/config/app/settings.ps1
+++ src/config/app/settings.ps1
@@ -1,3 +1,6 @@
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
+
 New-Variable -Name SdnDiagnostics -Scope Global -Force -Value @{
     Cache = @{}
     Credential = $null
