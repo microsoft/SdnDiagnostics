764acb8331d60e77e0e93bdd1a8f4db6ecbe22de
Adam Rudell <arudell@microsoft.com>
2021-08-12 15:32:22 -0500

    code fix

--- src/modules/private/utilities.ps1
+++ src/modules/private/utilities.ps1
@@ -735,7 +735,7 @@ function Remove-PSRemotingSession {
         [int]$timeOut = 120
         $stopWatch =  [System.Diagnostics.Stopwatch]::StartNew()
 
-        $sessions = Get-PSSession -Name "SdnDiag-*" | Where-Object {$_.ComputerName -ieq $ComputerName}
+        $sessions = Get-PSSession -Name "SdnDiag-*" | Where-Object {$_.ComputerName -iin $ComputerName}
         while($sessions){
             if($stopWatch.Elapsed.TotalSeconds -gt $timeOut){
                 throw New-Object System.TimeoutException("Unable to drain PSSessions")
