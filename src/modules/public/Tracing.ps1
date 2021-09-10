344b0fe4efc546aa3cf031911aff70a19c0a8237
Adam Rudell <arudell@microsoft.com>
2021-09-10 17:23:32 -0500

    add convert function

--- src/modules/public/Tracing.ps1
+++ src/modules/public/Tracing.ps1
@@ -45,7 +45,7 @@ function Disable-SdnGatewayTracing {
     }
 }
 
-function Enable-NetshTrace {
+function Start-NetshTrace {
     <#
     .SYNOPSIS
         Enables netsh tracing. Supports pre-configured trace providers or custom provider strings.
@@ -138,7 +138,7 @@ function Enable-NetshTrace {
     }
 }
 
-function Disable-NetshTrace {
+function Stop-NetshTrace {
     <#
     .SYNOPSIS
         Disables netsh tracing.
@@ -154,6 +154,74 @@ function Disable-NetshTrace {
 
     try {
         $result = Invoke-Expression -Command "netsh trace stop"
+    }
+    catch {
+        "{0}`n{1}" -f $_.Exception, $_.ScriptStackTrace | Trace-Output -Level:Error
+    }
+}
+
+function Convert-EtwTraceToTxt {
+    <#
+    .SYNOPSIS
+        Used to convert existing etw provider traces into text readable format
+    .PARAMETER FileName
+        ETL trace file path and name to convert 
+    .PARAMETER Destination
+        Output file name and directory. If ommitted, will use the FileName path and base name.
+    .PARAMETER Overwrite
+        Overwrites existing files. If ommitted, defaults to no.
+    .PARAMETER Report
+        Generates an HTML report. If ommitted, defaults to no.
+    #>
+
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory = $true)]
+        [System.IO.FileInfo]$FileName,
+
+        [Parameter(Mandatory = $false)]
+        [System.IO.FileInfo]$Destination,
+
+        [Parameter(Mandatory = $false)]
+        [ValidateSet('No','Yes')]
+        [System.String]$Overwrite = 'No',
+
+        [Parameter(Mandatory = $false)]
+        [ValidateSet('No','Yes')]
+        [System.String]$Report = 'No'
+    )
+
+    try {
+        if(!$Destination){
+            [System.IO.FileInfo]$Destination = $FileName.FullName
+        }
+
+        if(!(Test-Path -Path $Destination.FullName -PathType Container)){
+            $null = New-Item -Path $Destination.FullName -ItemType Directory -Force
+        }
+
+        [System.String]$outputFile = "{0}.txt" -f (Join-Path -Path $Destination.FullName -ChildPath $FileName.BaseName)
+        [System.String]$cmd = "netsh trace convert input={0} output={1} overwrite={2} report={3}" -f $FileName.FullName, $outputFile, $Overwrite, $Report
+        $convert = Invoke-Expression -Command "$cmd"
+
+        # output returned is string objects, so need to manually do some mapping to correlate the properties
+        # that can be then returned as psobject to the call
+        if($convert[5] -ieq 'Generating dump ... done'){
+            $object = New-Object -TypeName PSCustomObject -Property (
+                [Ordered]@{
+                    Status = 'Success'
+                    Input = $convert[1].Trim().Split('  ')[-1]
+                    Output = $convert[2].Trim().Split('  ')[-1]
+                    Format = 'txt'
+                }
+            )
+        }
+        else {
+            # typically, the first line returned in scenarios where there was an error thrown will contain the error details
+            throw New-Object System.Exception($convert[0])
+        }
+
+        return $object
     }
     catch {
         "{0}`n{1}" -f $_.Exception, $_.ScriptStackTrace | Trace-Output -Level:Error
