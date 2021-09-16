723e4be69a6c360e3d8d99db704f33d9bb6e5763
Luyao Feng <luyaof@microsoft.com>
2021-09-16 11:59:30 +0800

    fix issue that online tests use incorrect $Global variable name

--- tests/online/waveAll/Diagnostics.Tests.ps1
+++ tests/online/waveAll/Diagnostics.Tests.ps1
@@ -1,13 +1,13 @@
 # Pester tests
 Describe 'Debug-SdnFabricInfrastructure test' { 
     It "Debug-SdnFabricInfrastrucure run all debug with no exception" {
-        $debugResult = Debug-SdnFabricInfrastructure -NetworkController $Global:PesterGlobal.configdata.NcVM -NcRestCredential $Global:PesterGlobal.NcRestCredential
+        $debugResult = Debug-SdnFabricInfrastructure -NetworkController $Global:PesterOnlineTests.configdata.NcVM -NcRestCredential $Global:PesterOnlineTests.NcRestCredential
     }
   }
 
 Describe 'Test-SdnKnownIssue test' { 
     It "Test-SdnKnownIssue run all Known Issues test with no exception" {
-        $testKiResult = Test-SdnKnownIssue -NetworkController $Global:PesterGlobal.configdata.NcVM -NcRestCredential $Global:PesterGlobal.NcRestCredential
+        $testKiResult = Test-SdnKnownIssue -NetworkController $Global:PesterOnlineTests.configdata.NcVM -NcRestCredential $Global:PesterOnlineTests.NcRestCredential
     }
 }
 
--- tests/online/waveAll/Role.NetworkController.Tests.ps1
+++ tests/online/waveAll/Role.NetworkController.Tests.ps1
@@ -1,31 +1,31 @@
 # Pester tests
 Describe 'Get-SdnInfrastructureInfo test' { 
   It "Able to retreive NCUrl" {
-      $infraInfo = Get-SdnInfrastructureInfo -NetworkController $Global:PesterGlobal.configdata.NcVM -NcRestCredential $Global:PesterGlobal.NcRestCredential
+      $infraInfo = Get-SdnInfrastructureInfo -NetworkController $Global:PesterOnlineTests.configdata.NcVM -NcRestCredential $Global:PesterOfflineTests.NcRestCredential
       $infraInfo.NCUrl | Should -not -BeNullOrEmpty
   }
   It "All NC retrieved" {
-    $infraInfo = Get-SdnInfrastructureInfo -NetworkController $Global:PesterGlobal.configdata.NcVM -NcRestCredential $Global:PesterGlobal.NcRestCredential
-    $infraInfo.NC.Count | Should -Be $Global:PesterGlobal.ConfigData.NumberOfNc
+    $infraInfo = Get-SdnInfrastructureInfo -NetworkController $Global:PesterOnlineTests.configdata.NcVM -NcRestCredential $Global:PesterOfflineTests.NcRestCredential
+    $infraInfo.NC.Count | Should -Be $Global:PesterOnlineTests.ConfigData.NumberOfNc
   }
   It "All MUX retrieved" {
-    $infraInfo = Get-SdnInfrastructureInfo -NetworkController $Global:PesterGlobal.configdata.NcVM -NcRestCredential $Global:PesterGlobal.NcRestCredential
-    $infraInfo.Mux.Count | Should -Be $Global:PesterGlobal.ConfigData.NumberOfMux
+    $infraInfo = Get-SdnInfrastructureInfo -NetworkController $Global:PesterOnlineTests.configdata.NcVM -NcRestCredential $Global:PesterOfflineTests.NcRestCredential
+    $infraInfo.Mux.Count | Should -Be $Global:PesterOnlineTests.ConfigData.NumberOfMux
   }
   It "All Gateway retrieved" {
-    $infraInfo = Get-SdnInfrastructureInfo -NetworkController $Global:PesterGlobal.configdata.NcVM -NcRestCredential $Global:PesterGlobal.NcRestCredential
-    $infraInfo.Gateway.Count | Should -Be $Global:PesterGlobal.ConfigData.NumberOfGateway
+    $infraInfo = Get-SdnInfrastructureInfo -NetworkController $Global:PesterOnlineTests.configdata.NcVM -NcRestCredential $Global:PesterOfflineTests.NcRestCredential
+    $infraInfo.Gateway.Count | Should -Be $Global:PesterOnlineTests.ConfigData.NumberOfGateway
   }
 
   It "All Server retrieved" {
-    $infraInfo = Get-SdnInfrastructureInfo -NetworkController $Global:PesterGlobal.configdata.NcVM -NcRestCredential $Global:PesterGlobal.NcRestCredential
-    $infraInfo.Host.Count | Should -Be $Global:PesterGlobal.ConfigData.NumberOfServer
+    $infraInfo = Get-SdnInfrastructureInfo -NetworkController $Global:PesterOnlineTests.configdata.NcVM -NcRestCredential $Global:PesterOfflineTests.NcRestCredential
+    $infraInfo.Host.Count | Should -Be $Global:PesterOnlineTests.ConfigData.NumberOfServer
   }
 }
 
 Describe 'Get-SdnNetworkController test' {
     It "Able to retrieve Network Controller details"{
-        $NcInfo = Get-SdnNetworkController -NetworkController $Global:PesterGlobal.configdata.NcVM 
+        $NcInfo = Get-SdnNetworkController -NetworkController $Global:PesterOnlineTests.configdata.NcVM
         $NcInfo.Count | Should -Be 3
     }
 }
