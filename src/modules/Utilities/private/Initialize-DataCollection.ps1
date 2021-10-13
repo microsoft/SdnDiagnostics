4f867cdba39d67d464509be1b298b99fcfc72cf3
Adam Rudell <arudell@microsoft.com>
2021-10-13 17:07:53 -0500

    convert to warning vs exception

--- src/modules/Utilities/private/Initialize-DataCollection.ps1
+++ src/modules/Utilities/private/Initialize-DataCollection.ps1
@@ -15,7 +15,7 @@ function Initialize-DataCollection {
 
         [Parameter(Mandatory = $true, ParameterSetName = 'GB')]
         [Parameter(Mandatory = $true, ParameterSetName = 'MB')]
-        [System.String[]]$FilePath,
+        [System.IO.FileInfo]$FilePath,
 
         [Parameter(Mandatory = $true, ParameterSetName = 'GB')]
         [System.Int32]$MinimumGB,
@@ -28,41 +28,42 @@ function Initialize-DataCollection {
     if ($Role) {
         $config = Get-SdnRoleConfiguration -Role $Role
         $confirmFeatures = Confirm-RequiredFeaturesInstalled -Name $config.windowsFeature
-        if (!$confirmFeatures) {
-            throw New-Object System.NotSupportedException("Required feature is missing")
+        if (-NOT ($confirmFeatures)) {
+            "Required feature is missing: {0}" -f ($config.windowsFeature -join ', ') | Trace-Output -Level:Warning
+            return $false
         }
 
         $confirmModules = Confirm-RequiredModulesLoaded -Name $config.requiredModules
-        if (!$confirmModules) {
-            throw New-Object System.NotSupportedException("Required module is not loaded")
+        if (-NOT ($confirmModules)) {
+            "Required module is not loaded: {0}" -f ($config.requiredModules -join ', ')| Trace-Output -Level:Warning
+            return $false
         }
     }
 
     # create the directories if does not already exist
-    foreach ($path in $FilePath) {
-        if (!(Test-Path -Path $path -PathType Container)) {
-            $null = New-Item -Path $path -ItemType Directory -Force
-        }
-        else {
-            "{0} already exists. Performing cleanup operation" -f $path | Trace-Output -Level:Warning
-            Clear-SdnWorkingDirectory -Path $path -Force -Recurse
-        }
 
-        # confirm sufficient disk space
-        [System.Char]$driveLetter = (Split-Path -Path $path -Qualifier).Replace(':','')
+    if (-NOT (Test-Path -Path $FilePath.FullName -PathType Container)) {
+        $null = New-Item -Path $FilePath.FullName -ItemType Directory -Force
+    }
+    else {
+        "{0} already exists. Performing cleanup operation" -f $FilePath.FullName | Trace-Output
+        Clear-SdnWorkingDirectory -Path $FilePath.FullName -Force -Recurse
+    }
 
-        switch ($PSCmdlet.ParameterSetName) {
-            'GB' {
-                $diskSpace = Confirm-DiskSpace -DriveLetter $driveLetter -MinimumGB $MinimumGB
-            }
-            'MB' {
-                $diskSpace = Confirm-DiskSpace -DriveLetter $driveLetter -MinimumMB $MinimumMB
-            }
+    # confirm sufficient disk space
+    [System.Char]$driveLetter = (Split-Path -Path $FilePath.FullName -Qualifier).Replace(':','')
+    switch ($PSCmdlet.ParameterSetName) {
+        'GB' {
+            $diskSpace = Confirm-DiskSpace -DriveLetter $driveLetter -MinimumGB $MinimumGB
         }
+        'MB' {
+            $diskSpace = Confirm-DiskSpace -DriveLetter $driveLetter -MinimumMB $MinimumMB
+        }
+    }
 
-        if (!($diskSpace)) {
-            throw New-Object System.Exception("Insufficient disk space detected")
-        }
+    if (-NOT ($diskSpace)) {
+        "Insufficient disk space detected" | Trace-Output -Level:Warning
+        return $false
     }
 
     return $true
