9187aaa7fa3b82d2171c455791804752b0eb1cdc
Adam Rudell <arudell@microsoft.com>
2022-01-10 06:47:13 -0600

    User/arudell/add ps functions for mux drivercontrol console (#98)
    * add driver control function
    * add slb mux functions
    * add functions to exported list
    * updated code and function description template
    * update function descriptions
    * update config dump to use new functions
    * update to use json vs text for mux details

---
+++ src/modules/SoftwareLoadBalancer/public/Get-SdnMuxStatefulVip.ps1
@@ -0,0 +1,36 @@
+function Get-SdnMuxStatefulVip {
+    <#
+        .SYNOPSIS
+            Gets details related to the stateful VIPs.
+        .DESCRIPTION
+        .PARAMETER VirtualIP
+            The Virtual IP address (VIP) of the resource. If omitted, will return all VIPs programmed within the MUX driver.
+        .EXAMPLE
+            PS> Get-SdnMuxStatefulVip
+        .EXAMPLE
+            PS> Get-SdnMuxStatefulVip -VirtualIP 100.90.95.42
+    #>
+
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory = $false)]
+        [System.String]$VirtualIP
+    )
+
+    try {
+        $control = Get-MuxDriverControl
+        $statefulVips = [System.Collections.Generic.List[Microsoft.Cloudnet.Slb.Mux.Driver.VipEndpointKey]]::new()
+
+        $control.GetStatefulVips($null, [ref]$statefulVips)
+
+        if ($VirtualIP) {
+            return ($statefulVips | Where-Object {$_.AddressStr -ieq $VirtualIP})
+        }
+        else {
+            return $statefulVips
+        }
+    }
+    catch {
+        "{0}`n{1}" -f $_.Exception, $_.ScriptStackTrace | Trace-Output -Level:Error
+    }
+}
---
+++ src/modules/SoftwareLoadBalancer/public/Get-SdnMuxVipConfig.ps1
@@ -0,0 +1,42 @@
+function Get-SdnMuxVipConfig {
+    <#
+        .SYNOPSIS
+            Get configuration details such as the DIPs of the backend resources related to Virtual IP
+        .PARAMETER VirtualIP
+            The Virtual IP address (VIP) of the resource. If omitted, will return all VIPs programmed within the MUX driver.
+        .EXAMPLE
+            PS> Get-SdnMuxVipConfig
+        .EXAMPLE
+            PS> Get-SdnMuxVipConfig -VirtualIP 100.90.95.42
+    #>
+
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory = $false)]
+        [System.String]$VirtualIP
+    )
+
+    try {
+        $control = Get-MuxDriverControl
+        $list = [System.Collections.Generic.List[Microsoft.Cloudnet.Slb.Mux.Driver.VipEndpointConfig]]::new()
+
+        if ($VirtualIP) {
+            $statefulVips = Get-SdnMuxStatefulVip -VirtualIp $VirtualIP
+        }
+        else {
+            $statefulVips = Get-SdnMuxStatefulVip
+        }
+
+        foreach ($vip in $statefulVips) {
+            $vipConfig = New-Object -Type Microsoft.Cloudnet.Slb.Mux.Driver.VipEndpointConfig
+            $control.GetVipConfig($vip, [ref]$vipConfig)
+
+            [void]$list.Add($vipConfig)
+        }
+
+        return $list
+    }
+    catch {
+        "{0}`n{1}" -f $_.Exception, $_.ScriptStackTrace | Trace-Output -Level:Error
+    }
+}
