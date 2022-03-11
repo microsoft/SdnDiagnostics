fcfe395c14615ed3afd74e8c4731503e9acc5d8e
Adam Rudell <arudell@microsoft.com>
2022-03-11 10:48:08 -0600

    change messaging

--- src/modules/Utilities/private/Copy-FileFromRemoteComputerSMB.ps1
+++ src/modules/Utilities/private/Copy-FileFromRemoteComputerSMB.ps1
@@ -59,7 +59,7 @@ function Copy-FileFromRemoteComputerSMB {
     foreach ($subPath in $Path) {
         $remotePath = Convert-FileSystemPathToUNC -ComputerName $ComputerName -Path $subPath
         if (-NOT (Test-Path -Path $remotePath)) {
-            "Unable to locate {0}" -f $remotePath | Trace-Output -Level:Error
+            "Unable to find {0}" -f $remotePath | Trace-Output -Level:Error
         }
         else {
             [void]$arrayList.Add($remotePath)
