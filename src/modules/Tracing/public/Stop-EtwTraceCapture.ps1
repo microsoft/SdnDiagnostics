741683d549b055c0278270984933ef3b5ce9aadd
Adam Rudell <arudell@microsoft.com>
2022-06-16 14:24:08 -0500

    update to use exception level

--- src/modules/Tracing/public/Stop-EtwTraceCapture.ps1
+++ src/modules/Tracing/public/Stop-EtwTraceCapture.ps1
@@ -6,7 +6,7 @@ function Stop-EtwTraceCapture {
     .SYNOPSIS
         Start ETW Trace capture based on Role
     .PARAMETER Role
-        The SDN Roles 
+        The SDN Roles
     .PARAMETER Providers
         Allowed values are Default,Optional And All to control what are the providers needed
     #>
@@ -24,12 +24,12 @@ function Stop-EtwTraceCapture {
 
     try {
         $traceProvidersArray = Get-TraceProviders -Role $Role -Providers $Providers
-    
+
         foreach ($traceProviders in $traceProvidersArray) {
             Stop-EtwTraceSession -TraceName $traceProviders.name
         }
     }
     catch {
-        "{0}`n{1}" -f $_.Exception, $_.ScriptStackTrace | Trace-Output -Level:Error
+        "{0}`n{1}" -f $_.Exception, $_.ScriptStackTrace | Trace-Output -Level:Exception
     }
 }
