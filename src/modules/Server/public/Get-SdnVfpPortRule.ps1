10d2bffbe2ad2a47e9c0c9e363efac2250a2c25d
Adam Rudell <arudell@microsoft.com>
2022-10-03 17:43:35 -0500

    change how rule processing is handled

--- src/modules/Server/public/Get-SdnVfpPortRule.ps1
+++ src/modules/Server/public/Get-SdnVfpPortRule.ps1
@@ -49,96 +49,107 @@ function Get-SdnVfpPortRule {
         }
 
         foreach ($line in $vfpRules) {
+            $line = $line.Trim()
 
-            # in situations where the value might be nested in another line we need to do some additional data processing
-            # subvalues is declared below if the value is null after the split
-            if ($subValues) {
-                if (!$subArrayList) {
+            if ($line -like 'ITEM LIST' -or $line -ilike '==========='){
+                continue
+            }
+
+            if ([string]::IsNullOrEmpty($line)) {
+                continue
+            }
+
+            if ($subKey) {
+                if($null -eq $subObject){
+                    $subObject = New-Object -TypeName PSObject
+                }
+                if ($null -eq $subArrayList) {
                     $subArrayList = [System.Collections.ArrayList]::new()
                 }
 
-                # if we hit here, we have captured all of the conditions within the group that need processing
-                # and we can now add the arraylist to the object and null out the values
-                if ($line.Contains('Match type')) {
-                    $object | Add-Member -NotePropertyMembers $subArrayList -TypeName $key
+                switch ($subKey) {
+                    'Conditions' {
+                        # this will have a pattern of multiple lines nested under Conditions: in which we see a pattern of property:value format
+                        # we also see common pattern that Flow TTL is the next property after Conditions, so we can use that to determine when
+                        # no further processing is needed for this sub value
+                        if ($line.Contains('Flow TTL')) {
+                            $object | Add-Member -NotePropertyMembers @{Conditions = $subObject}
 
-                    $subValues = $false
-                    $subArrayList = $null
+                            $subObject = $null
+                            $subKey = $null
+                        }
+                        else {
+                            [System.String[]]$subResults = $line.Split(':').Trim()
+                            $subObject | Add-Member -MemberType NoteProperty -Name $subResults[0] -Value $subResults[1]
+                        }
+                    }
+                    'Encap Destination(s)' {
+                        # we typically see a format pattern of {property=value,property=value} for encap destination
+                        # and should be contained all within a single line. we also see a matching pattern that FlagsEx is the next property result
+                        # so we can use that to determine when no further processing is needed for this sub value
+                        if ($line.Contains('FlagsEx')) {
+                            $object | Add-Member -MemberType NoteProperty -Name 'Encap Destination' -Value $subObject
+
+                            $subObject = $null
+                            $subKey = $null
+                        }
+                        else {
+                            [System.String[]]$subResults = $line.Replace('{','').Replace('}','').Split(',').Trim()
+                            foreach ($subResult in $subResults) {
+                                [System.String]$subKeyName = $subResult.Split('=')[0].Trim()
+                                [System.String]$subKeyValue = $subResult.Split('=')[1].Trim()
+
+                                $subObject | Add-Member -MemberType NoteProperty -Name $subKeyName -Value $subKeyValue
+                            }
+                        }
+                    }
                 }
-                else {
-                    if ($line.Contains(':')) {
-                        [void]$subArrayList.Add($line.trim())
-                        continue
-                    }
-                    elseif ($line.Contains('<none>')) {
-                        $object | Add-Member -MemberType NoteProperty -Name $key -Value $null
 
-                        $subValues = $false
-                        $subArrayList = $null
-
-                        continue
-                    }
-                    else {
-                        $object | Add-Member -MemberType NoteProperty -Name $key -Value $line.Trim()
-
-                        $subValues = $false
-                        $subArrayList = $null
-
-                        continue
-                    }
-                }
+                # since we are processing sub values, we want to move to the next line and not do any further processing
+                continue
             }
 
             # lines in the VFP output that contain : contain properties and values
             # need to split these based on count of ":" to build key and values
             if ($line.Contains(':')) {
-                [System.String[]]$results = $line.Split(':').Trim()
+                [System.String[]]$results = $line.Split(':')
                 if ($results.Count -eq 2) {
-                    $key = $results[0]
+                    [System.String]$key = $results[0].Trim()
+                    [System.String]$value = $results[1].Trim()
 
                     # all groups begin with this property and value so need to create a new psobject when we see these keys
                     if ($key -ieq 'RULE') {
+                        if ($object) {
+                            [void]$arrayList.Add($object)
+                        }
+
                         $object = New-Object -TypeName PSObject
+                        $object | Add-Member -MemberType NoteProperty -Name $key -Value $value
+
+                        continue
+                    }
+
+                    if ($key -ieq 'Conditions' -or $key -ieq 'Encap Destination(s)') {
+                        $subKey = $key
                         continue
                     }
 
                     # if the key is priority, we want to declare the value as an int value so we can properly sort the results
-                    if ($key -ieq 'Priority') {
-                        [int]$value = $results[1]
-                    }
-                    else {
-
-                        # if we split the object and the second object is null or white space
-                        # we can assume that the lines below it have additional data we need to capture and as such
-                        # need to do further processing
-                        if ([string]::IsNullOrWhiteSpace($results[1])) {
-                            $subValues = $true
-                            continue
-                        }
-
-                        [System.String]$value = $results[1]
+                    elseif ($key -ieq 'Priority') {
+                        [int]$value = $value
                     }
                 }
 
                 # add the line values to the object
                 $object | Add-Member -MemberType NoteProperty -Name $key -Value $value
             }
-
-            # all the groups are seperated with a blank line
-            # use this as our end of properties to add the current obj to the array list
-            if ([string]::IsNullOrEmpty($line)) {
-                if ($object) {
-                    [void]$arrayList.Add($object)
-                }
-            }
         }
 
         if ($Name) {
-            return ($arrayList | Where-Object {$_.'Friendly name' -ieq $Name})
+            return ($arrayList | Where-Object {$_.'RULE' -ieq $Name})
         }
 
         return ($arrayList | Sort-Object -Property Priority)
-
     }
     catch {
         "{0}`n{1}" -f $_.Exception, $_.ScriptStackTrace | Trace-Output -Level:Error
