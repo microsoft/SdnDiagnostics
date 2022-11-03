2e5eabec06aa0189d3d3b302b27460b0fd5002d3
Adam Rudell <arudell@microsoft.com>
2022-11-02 23:46:31 -0500

    add fallback method for if NC certs are expired

---
+++ src/modules/NetworkController/private/Get-NetworkControllerNodeInfoFromClusterManifest.ps1
@@ -0,0 +1,40 @@
+function Get-NetworkControllerNodeInfoFromClusterManifest {
+    <#
+    .SYNOPSIS
+        This function is used as fallback method in the event that normal Get-NetworkControllerNode cmdlet fails in scenarios where certs may be expired
+    #>
+
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory = $false)]
+        [System.Management.Automation.PSCredential]
+        [System.Management.Automation.Credential()]
+        $Credential = [System.Management.Automation.PSCredential]::Empty
+    )
+
+    "Attempting to retrieve NetworkControllerNode information via ClusterManifest and other methods" | Trace-Output
+    $array = @()
+
+    $clusterManifest = [xml](Get-SdnServiceFabricClusterManifest)
+    $clusterManifest.ClusterManifest.Infrastructure.WindowsServer.NodeList.Node | ForEach-Object {
+        $object = [PSCustomObject]@{
+            Name = $_.NodeName
+            Server = $_.IPAddressOrFQDN
+            FaultDomain = $_.FaultDomain
+        }
+
+        $object | Add-Member -MemberType NoteProperty -Name RestInterface -Value 'test'
+        $object | Add-Member -MemberType NoteProperty -Name Status -Value (Get-SdnServiceFabricNode -Node $_.NodeName).NodeStatus
+
+	    $certificate = ($clusterManifest.ClusterManifest.NodeTypes.NodeType | Where-Object Name -ieq $_.NodeName).Certificates.ServerCertificate.X509FindValue.ToString()
+        $cert = Invoke-PSRemoteCommand -ComputerName $_.IPAddressOrFQDN -Credential $Credential -ScriptBlock {
+            Get-SdnCertificate -Path 'Cert:\LocalMachine\My' -Thumbprint $using:certificate
+        }
+
+        $object | Add-Member -MemberType NoteProperty -Name NodeCertificate -Value $cert
+
+        $array += $object
+    }
+
+    return $array
+}
--- src/modules/NetworkController/public/Get-SdnServiceFabricNode.ps1
+++ src/modules/NetworkController/public/Get-SdnServiceFabricNode.ps1
@@ -9,6 +9,8 @@ function Get-SdnServiceFabricNode {
         Specifies the name of the network controller node on which this cmdlet operates.
     .PARAMETER Credential
         Specifies a user account that has permission to perform this action. The default is the current user.
+    .PARAMETER NodeName
+        Specifies the name of the Service Fabric node whose information is being returned. If not specified, the cmdlet will return information for all the nodes in the cluster.
     .EXAMPLE
         PS> Get-SdnServiceFabricNode -NetworkController 'Prefix-NC01' -Credential (Get-Credential)
     #>
@@ -16,21 +18,32 @@ function Get-SdnServiceFabricNode {
     [CmdletBinding()]
     param (
         [Parameter(Mandatory = $false)]
-        [System.String[]]$NetworkController = $global:SdnDiagnostics.EnvironmentInfo.NetworkController,
+        [System.String[]]$NetworkController = $env:COMPUTERNAME,
 
         [Parameter(Mandatory = $false)]
         [System.Management.Automation.PSCredential]
         [System.Management.Automation.Credential()]
-        $Credential = [System.Management.Automation.PSCredential]::Empty        
+        $Credential = [System.Management.Automation.PSCredential]::Empty,
+
+        [Parameter(Mandatory = $false)]
+        [System.String]$NodeName
+
     )
 
     try {
-        if ($NetworkController) {
-            Invoke-SdnServiceFabricCommand -NetworkController $NetworkController -ScriptBlock { Get-ServiceFabricNode } -Credential $Credential
+
+        if ($NodeName) {
+            $sb = {
+                Get-ServiceFabricNode -NodeName $using:NodeName
+            }
         }
         else {
-            Invoke-SdnServiceFabricCommand -ScriptBlock { Get-ServiceFabricNode } -Credential $Credential
+            $sb = {
+                Get-ServiceFabricNode
+            }
         }
+
+        Invoke-SdnServiceFabricCommand -NetworkController $NetworkController -ScriptBlock $sb -Credential $Credential
     }
     catch {
         "{0}`n{1}" -f $_.Exception, $_.ScriptStackTrace | Trace-Output -Level:Error
