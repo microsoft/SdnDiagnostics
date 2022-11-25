dcc453245e55fd51bed48507430990c8d2633680
Adam Rudell <arudell@microsoft.com>
2022-11-25 10:25:05 -0600

    add additional API endpoints

--- src/enum/SdnApiResource.ps1
+++ src/enum/SdnApiResource.ps1
@@ -3,23 +3,30 @@
 
 enum SdnApiResource {
     AccessControlLists
+    AuditingSettingsConfig
     Credentials
+    Discovery
     GatewayPools
     Gateways
     iDNSServerConfig
+    LearnedIPAddresses
     LoadBalancerManagerConfig
     LoadBalancerMuxes
     LoadBalancers
     LogicalNetworks
     MacPools
     NetworkControllerState
+    NetworkControllerStatistics
     NetworkInterfaces
     PublicIPAddresses
+    SecurityTags
     Servers
+    ServiceInsertions
     SlbState
     RouteTables
     VirtualGateways
     VirtualNetworkManagerConfig
     VirtualNetworks
     VirtualServers
+    VirtualSwitchManagerConfig
 }
--- src/modules/Common/private/Get-SdnApiEndpoint.ps1
+++ src/modules/Common/private/Get-SdnApiEndpoint.ps1
@@ -27,19 +27,24 @@ function Get-SdnApiEndpoint {
     try {
         $apiEndpoints = @{
             AccessControlLists = "networking/{0}/accessControlLists" -f $ApiVersion
+            AuditingSettingsConfig = "networking/{0}/auditingSettings/configuration" -f $ApiVersion
             Credentials = "networking/{0}/credentials" -f $ApiVersion
             Discovery = "networking/discovery"
             GatewayPools = "networking/{0}/gatewayPools" -f $ApiVersion
             Gateways = "networking/{0}/gateways" -f $ApiVersion
             iDNSServerConfig = "networking/{0}/iDNSServer/configuration" -f $ApiVersion
+            LearnedIPAddresses ="/networking/{0}/learnedIpAddresses" -f $ApiVersion
             LoadBalancerManagerConfig = "networking/{0}/loadBalancerManager/config" -f $ApiVersion
             LoadBalancerMuxes = "networking/{0}/loadBalancerMuxes" -f $ApiVersion
             LoadBalancers = "networking/{0}/loadBalancers" -f $ApiVersion
             LogicalNetworks = "networking/{0}/logicalNetworks" -f $ApiVersion
             MacPools = "networking/{0}/macPools" -f $ApiVersion
             NetworkControllerState = "networking/{0}/diagnostics/networkControllerState" -f $ApiVersion
+            NetworkControllerStatistics = "networking/{0}/monitoring/networkControllerStatistics" -f $ApiVersion
             NetworkInterfaces = "networking/{0}/networkInterfaces" -f $ApiVersion
             PublicIPAddresses = "networking/{0}/publicIPAddresses" -f $ApiVersion
+            SecurityTags = "networking/{0}/securityTags" -f $ApiVersion
+            ServiceInsertions = "networking/{0}/serviceInsertions" -f $ApiVersion
             Servers = "networking/{0}/servers" -f $ApiVersion
             SlbState = "networking/{0}/diagnostics/slbState" -f $ApiVersion
             SlbStateResults = "networking/{0}/diagnostics/slbStateResults/{1}" -f $ApiVersion, $OperationId
@@ -48,6 +53,7 @@ function Get-SdnApiEndpoint {
             VirtualNetworkManagerConfig = "networking/{0}/virtualNetworkManager/configuration" -f $ApiVersion
             VirtualNetworks = "networking/{0}/virtualNetworks" -f $ApiVersion
             VirtualServers = "networking/{0}/virtualServers" -f $ApiVersion
+            VirtualSwitchManagerConfig = "networking/{0}/virtualSwitchManager/configuration" -f $ApiVersion
         }
 
         if($PSBoundParameters.ContainsKey('ResourceRef')){
