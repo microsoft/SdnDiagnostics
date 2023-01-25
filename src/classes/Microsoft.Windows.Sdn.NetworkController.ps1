46bdbd060abf1ae43dbb7c7c90fc890c7ad27bf4
Adam Rudell <arudell@microsoft.com>
2023-01-25 11:01:33 -0600

    rename SoftwareLoadBalancer

--- src/classes/Microsoft.Windows.Sdn.NetworkController.ps1
+++ src/classes/Microsoft.Windows.Sdn.NetworkController.ps1
@@ -1,6 +1,6 @@
 class SdnFabricInfrastructure {
     [System.String[]]$NetworkController
-    [System.String[]]$SoftwareLoadBalancer
+    [System.String[]]$LoadBalancerMux
     [System.String[]]$Gateway
     [System.String]$NcUrl
     [System.String]$RestApiVersion
