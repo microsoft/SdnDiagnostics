850b6eee019ade3c8e71ad935d7c17ff6d5fc68d
Adam Rudell <arudell@microsoft.com>
2023-01-26 20:01:00 -0600

    User/arudell/create arg completers for sf services (#167)
    - add argument completers for sf cmdlets for servicetypename and servicename
    - added failsafe to prevent move replica when less than 3 NC nodes

--- src/config/ArgumentCompleters.ps1
+++ src/config/ArgumentCompleters.ps1
@@ -75,6 +75,48 @@ $scriptBlocks = @{
 
         return $testName | Where-Object {$_ -like "*$wordToComplete*"} | Sort-Object
     }
+
+    ServiceFabricServiceName = {
+        param($commandName, $parameterName, $wordToComplete, $commandAst, $fakeBoundParameters)
+        $serviceName = @(
+            'fabric:/NetworkController/ApiService'
+            'fabric:/NetworkController/BackupRestore'
+            'fabric:/NetworkController/ControllerService'
+            'fabric:/NetworkController/FirewallService'
+            'fabric:/NetworkController/FnmService'
+            'fabric:/NetworkController/GatewayManager'
+            'fabric:/NetworkController/HelperService'
+            'fabric:/NetworkController/ServiceInsertion'
+            'fabric:/NetworkController/SlbManagerService'
+            'fabric:/NetworkController/UpdateService'
+            'fabric:/NetworkController/VSwitchService'
+        )
+
+        if ([string]::IsNullOrEmpty($wordToComplete)) {
+            return ($serviceName | Sort-Object)
+        }
+    }
+
+    ServiceFabricServiceTypeName = {
+        param($commandName, $parameterName, $wordToComplete, $commandAst, $fakeBoundParameters)
+        $serviceTypeName = @(
+            'ApiService'
+            'BackupRestore'
+            'ControllerService'
+            'FirewallService'
+            'FnmService'
+            'GatewayManager'
+            'HelperService'
+            'ServiceInsertion'
+            'SlbManagerService'
+            'UpdateService'
+            'VSwitchService'
+        )
+
+        if ([string]::IsNullOrEmpty($wordToComplete)) {
+            return ($serviceTypeName | Sort-Object)
+        }
+    }
 }
 
 Register-ArgumentCompleter -CommandName 'Get-SdnKnownIssue' -ParameterName 'Name' -ScriptBlock $scriptBlocks.KnownIssueTests
@@ -123,3 +165,15 @@ $serverParamCommands = (
 
 Register-ArgumentCompleter -CommandName $serverParamCommands -ParameterName 'ComputerName' -ScriptBlock $scriptBlocks.ServerNodes
 
+# argument completers for service fabric cmdlets
+Register-ArgumentCompleter -CommandName 'Get-SdnServiceFabricReplica' -ParameterName 'ServiceName' -ScriptBlock $scriptBlocks.ServiceFabricServiceName
+Register-ArgumentCompleter -CommandName 'Get-SdnServiceFabricReplica' -ParameterName 'ServiceTypeName' -ScriptBlock $scriptBlocks.ServiceFabricServiceTypeName
+
+Register-ArgumentCompleter -CommandName 'Get-SdnServiceFabricService' -ParameterName 'ServiceName' -ScriptBlock $scriptBlocks.ServiceFabricServiceName
+Register-ArgumentCompleter -CommandName 'Get-SdnServiceFabricService' -ParameterName 'ServiceTypeName' -ScriptBlock $scriptBlocks.ServiceFabricServiceTypeName
+
+Register-ArgumentCompleter -CommandName 'Get-SdnServiceFabricPartition' -ParameterName 'ServiceName' -ScriptBlock $scriptBlocks.ServiceFabricServiceName
+Register-ArgumentCompleter -CommandName 'Get-SdnServiceFabricPartition' -ParameterName 'ServiceTypeName' -ScriptBlock $scriptBlocks.ServiceFabricServiceTypeName
+
+Register-ArgumentCompleter -CommandName 'Move-SdnServiceFabricReplica' -ParameterName 'ServiceName' -ScriptBlock $scriptBlocks.ServiceFabricServiceName
+Register-ArgumentCompleter -CommandName 'Move-SdnServiceFabricReplica' -ParameterName 'ServiceTypeName' -ScriptBlock $scriptBlocks.ServiceFabricServiceTypeName
