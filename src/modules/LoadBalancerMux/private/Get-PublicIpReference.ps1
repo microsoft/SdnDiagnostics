75fad53fa5221ef518a9f3ec73200a97510029b4
Adam Rudell <arudell@microsoft.com>
2023-02-02 08:40:15 -0600

    improve logging for debug purposes (#169)

--- src/modules/LoadBalancerMux/private/Get-PublicIpReference.ps1
+++ src/modules/LoadBalancerMux/private/Get-PublicIpReference.ps1
@@ -19,17 +19,21 @@ function Get-PublicIpReference {
 
     try {
 
-        # NIC is directly connected to a Public IP
+        # check for an instance-level public IP address that is directly associated
+        # with the ipconfiguration and return back to calling function
         if ($IpConfiguration.properties.publicIPAddress) {
-            "Located public IP address attached to {0}" -f $IpConfiguration.resourceRef | Trace-Output -Level:Verbose
+            "Located {0} associated with {1}" -f $IpConfiguration.properties.publicIPAddress.resourceRef, $IpConfiguration.resourceRef | Trace-Output -Level:Verbose
             return ($IpConfiguration.properties.publicIPAddress.resourceRef)
+        }
+        else {
+            "Unable to locate an instance-level public IP address associated with {0}" -f $IpConfiguration.resourceRef | Trace-Output -Level:Verbose
         }
 
         # NIC is connected to a load balancer with public IP association
-        # or NIC is not associated to a public IP by any means and instead is connected via implicit load balancer attached
-        # to a virtual network
+        # or NIC is not associated to a public IP by any means and instead is connected via implicit load balancer attached to a virtual network
+        "Checking for any backend address pool associated with {0}" -f $IpConfiguration.resourceRef | Trace-Output -Level:Verbose
         if ($IpConfiguration.properties.loadBalancerBackendAddressPools) {
-            "Located backend address pool association for {0}" -f $IpConfiguration.resourceRef | Trace-Output -Level:Verbose
+            "Located backend address pool associations for {0}" -f $IpConfiguration.resourceRef | Trace-Output -Level:Verbose
             $loadBalancers = Get-SdnResource -NcUri $NcUri.AbsoluteUri -Resource:LoadBalancers -Credential $Credential
             $allBackendPoolRefs = @($IpConfiguration.properties.loadBalancerBackendAddressPools.resourceRef)
 
@@ -39,10 +43,11 @@ function Get-PublicIpReference {
             }
 
             foreach ($backendPoolRef in $allBackendPoolRefs) {
+                "Checking for outboundNatRules for {0}" -f $backendPoolRef | Trace-Output -Level:Verbose
                 $bePool = $backendHash[$backendPoolRef]
 
                 if ($bePool.properties.outboundNatRules) {
-                    "Located outbound NAT rule associated with {0}" -f $bePool.resourceRef | Trace-Output -Level:Verbose
+                    "Located outboundNatRule associated with {0}" -f $bePool.resourceRef | Trace-Output -Level:Verbose
 
                     $obRuleRef = $bePool.properties.outboundNatRules[0].resourceRef
                     break
@@ -53,9 +58,15 @@ function Get-PublicIpReference {
                 $natRule = $loadBalancers.properties.outboundNatRules | Where-Object { $_.resourceRef -eq $obRuleRef }
                 $frontendConfig = $loadBalancers.properties.frontendIPConfigurations | Where-Object { $_.resourceRef -eq $natRule.properties.frontendIPConfigurations[0].resourceRef }
 
-                "Located {0} associated with outbound NAT rule {0}" -f $frontendConfig.resourceRef, $natRule.resourceRef | Trace-Output -Level:Verbose
+                "Located {0} associated with {0}" -f $frontendConfig.resourceRef, $natRule.resourceRef | Trace-Output -Level:Verbose
                 return ($frontendConfig.properties.publicIPAddress.resourceRef)
             }
+            else {
+                "Unable to locate outboundNatRules associated with {0}" -f $IpConfiguration.properties.loadBalancerBackendAddressPools.resourceRef | Trace-Output -Level:Verbose
+            }
+        }
+        else {
+            "Unable to locate any backend pools associated with {0}" -f $IpConfiguration.resourceRef | Trace-Output -Level:Verbose
         }
 
         return $null
