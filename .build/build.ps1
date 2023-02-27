d6faaf4318ecd68e2f5e7dd418d771d664e6651b
Adam Rudell <arudell@microsoft.com>
2023-02-27 14:16:17 -0600

    define nuspec and define nuget config

--- .build/build.ps1
+++ .build/build.ps1
@@ -1,36 +1,30 @@
+param (
+    [String]$Version
+)
+
 $currentErrorPref = $ErrorActionPreference
 $ErrorActionPreference = 'Stop'
 
-& "$PSScriptRoot\validate-functions.ps1"
-& "$PSScriptRoot\generate-version.ps1"
-& "$PSScriptRoot\clean.ps1"
-
-function Get-ModuleVersion {
-    # this is set in a prior step from generateVersion-ps.ps1
-    return $env:SdnDiagCustomBuildNumber
-}
 
 $outDir = "$PSScriptRoot\..\out\build"
 if (-NOT (Test-Path -Path $outDir -PathType Container)) {
     $null = New-Item -ItemType:Directory -Path $outDir -Force
 }
 
-Copy-Item -Path "$PSScriptRoot\..\src" -Destination "$outDir\SdnDiagnostics" -Exclude "*.md" -Recurse -Force
-& $PSScriptRoot\restore.ps1 -DestinationFolder "$outDir\SdnDiagnostics"
+Copy-Item -Path "$PSScriptRoot\..\src" -Destination "$outDir\SdnDiagnostics" -Recurse -Force
 
 # setting the version of the module manifest
 $modManifest = Get-ChildItem "$outDir\SdnDiagnostics" -Filter "*.psd1"
 if (($null -ne (Get-Item -Path "$($modManifest.DirectoryName)\$($modManifest.BaseName).psm1" -ErrorAction SilentlyContinue))) {
     try {
-        $modVersion = Get-ModuleVersion
         $manifest = Test-ModuleManifest -Path $modManifest.FullName
 
-        if ($manifest.Version.ToString() -ne $modVersion) {
-            "`r`nUpdating {0} version: {1} --> {2}" -f $modManifest.BaseName, $manifest.Version.ToString(), $modVersion | Write-Host
-            Update-ModuleManifest -ModuleVersion $modVersion -Path $modManifest.FullName
+        if ($manifest.Version.ToString() -ne $Version) {
+            "Updating {0} version: {1} --> {2}" -f $modManifest.BaseName, $manifest.Version.ToString(), $Version | Write-Host
+            Update-ModuleManifest -ModuleVersion $Version -Path $modManifest.FullName
         }
         else {
-            "`r`n{0} version does not need to be updated." -f $modVersion | Write-Host
+            "`r`n{0} version does not need to be updated." -f $Version | Write-Host
         }
     }
     catch {
---
+++ build.ps1
@@ -0,0 +1,18 @@
+if (Get-Module -Name 'SdnDiagnostics') {
+    Remove-Module -Name 'SdnDiagnostics' -Force
+}
+
+.$PSScriptRoot\.build\clean.ps1
+.$PSScriptRoot\.build\restore.ps1
+$buildVersion = .$PSScriptRoot\.build\generate-Version.ps1
+if ($buildVersion) {
+    .$PSScriptRoot\.build\build.ps1 -Version $buildVersion
+    .$PSScriptRoot\.build\package.ps1 -Version $buildVersion
+}
+
+# copy the contents from .packages to the out\build folder
+foreach ($item in (Get-ChildItem -Path $PSScriptRoot\.packages -Directory)) {
+    $restorePath = Get-Item -Path $PSScriptRoot\out\build\SdnDiagnostics
+    "Copying {0} to {1}" -f $item.BaseName, $restorePath.FullName | Write-Host
+    Copy-Item -Path $item.FullName -Destination (Join-Path -Path $restorePath -ChildPath "packages\$($item.BaseName)") -Recurse -Exclude *.nupkg
+}
