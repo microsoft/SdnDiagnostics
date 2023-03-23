525e740fcf9d202cb509b54e401d4ab4703b5900
Luyao Feng <luyaof@microsoft.com>
2023-03-23 14:20:58 +0800

    fix issue for get-sdnaidutlog when audit log not enabled

--- src/modules/SdnDiag.NetworkController/public/Get-SdnAuditLog.ps1
+++ src/modules/SdnDiag.NetworkController/public/Get-SdnAuditLog.ps1
@@ -37,41 +37,37 @@ function Get-SdnAuditLog {
         $Credential = [System.Management.Automation.PSCredential]::Empty
     )
 
-    begin {
-        # verify that the environment we are on supports at least v3 API and later
-        # as described in https://learn.microsoft.com/en-us/openspecs/windows_protocols/ms-ncnbi/dc23b547-9ec4-4cb3-ab20-a6bfe01ddafb
-        $currentRestVersion = (Get-SdnResource -NcUri $NcUri.AbsoluteUri -Resource 'Discovery' -Credential $NcRestCredential).properties.currentRestVersion
-        [int]$currentRestVersionInt = $currentRestVersion.Replace('V','').Replace('v','').Trim()
-        if ($currentRestVersionInt -lt 3) {
-            "Auditing requires API version 3 or later. Network Controller supports version {0}" -f $currentRestVersionInt | Trace-Output -Level:Warning
-            return
-        }
-
-        # check to see that auditing has been enabled
-        $auditSettingsConfig = Get-SdnResource -NcUri $NcUri.AbsoluteUri -Resource 'AuditingSettingsConfig' -ApiVersion $currentRestVersion -Credential $NcRestCredential
-        if ([string]::IsNullOrEmpty($auditSettingsConfig.properties.outputDirectory)) {
-            "Audit logging is not enabled" | Trace-Output
-            return
-        }
-        else {
-            "Audit logging location: {0}" -f $auditSettingsConfig.properties.outputDirectory | Trace-Output
-        }
-
-        # if $ComputerName was not specified, then attempt to locate the servers within the SDN fabric
-        # only add the servers where auditingEnabled has been configured as 'Firewall'
-        if ($null -eq $ComputerName) {
-            $sdnServers = Get-SdnResource -Resource Servers -NcUri $NcUri.AbsoluteUri -Credential $NcRestCredential -ApiVersion $currentRestVersion `
-            | Where-Object {$_.properties.auditingEnabled -ieq 'Firewall'}
-
-            $ComputerName = ($sdnServers.properties.connections | Where-Object {$_.credentialType -ieq 'UsernamePassword'}).managementAddresses
-        }
+    # verify that the environment we are on supports at least v3 API and later
+    # as described in https://learn.microsoft.com/en-us/openspecs/windows_protocols/ms-ncnbi/dc23b547-9ec4-4cb3-ab20-a6bfe01ddafb
+    $currentRestVersion = (Get-SdnResource -NcUri $NcUri.AbsoluteUri -Resource 'Discovery' -Credential $NcRestCredential).properties.currentRestVersion
+    [int]$currentRestVersionInt = $currentRestVersion.Replace('V','').Replace('v','').Trim()
+    if ($currentRestVersionInt -lt 3) {
+        "Auditing requires API version 3 or later. Network Controller supports version {0}" -f $currentRestVersionInt | Trace-Output -Level:Warning
+        return
     }
 
-    process {
-        $ComputerName | ForEach-Object {
-            "Collecting audit logs from {0}" -f $_ | Trace-Output
-            $outputDir = Join-Path -Path $OutputDirectory -ChildPath $_.ToLower()
-            Copy-FileFromRemoteComputer -ComputerName $_ -Credential $Credential -Path $auditSettingsConfig.properties.outputDirectory -Destination $outputDir -Recurse -Force
-        }
+    # check to see that auditing has been enabled
+    $auditSettingsConfig = Get-SdnResource -NcUri $NcUri.AbsoluteUri -Resource 'AuditingSettingsConfig' -ApiVersion $currentRestVersion -Credential $NcRestCredential
+    if ([string]::IsNullOrEmpty($auditSettingsConfig.properties.outputDirectory)) {
+        "Audit logging is not enabled" | Trace-Output
+        return
+    }
+    else {
+        "Audit logging location: {0}" -f $auditSettingsConfig.properties.outputDirectory | Trace-Output
+    }
+
+    # if $ComputerName was not specified, then attempt to locate the servers within the SDN fabric
+    # only add the servers where auditingEnabled has been configured as 'Firewall'
+    if ($null -eq $ComputerName) {
+        $sdnServers = Get-SdnResource -Resource Servers -NcUri $NcUri.AbsoluteUri -Credential $NcRestCredential -ApiVersion $currentRestVersion `
+        | Where-Object {$_.properties.auditingEnabled -ieq 'Firewall'}
+
+        $ComputerName = ($sdnServers.properties.connections | Where-Object {$_.credentialType -ieq 'UsernamePassword'}).managementAddresses
+    }
+
+    $ComputerName | ForEach-Object {
+        "Collecting audit logs from {0}" -f $_ | Trace-Output
+        $outputDir = Join-Path -Path $OutputDirectory -ChildPath $_.ToLower()
+        Copy-FileFromRemoteComputer -ComputerName $_ -Credential $Credential -Path $auditSettingsConfig.properties.outputDirectory -Destination $outputDir -Recurse -Force
     }
 }
