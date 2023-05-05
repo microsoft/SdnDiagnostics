d9965309b498b54723c55074d311c3ec65827ed8
Adam Rudell <arudell@microsoft.com>
2023-05-05 09:53:31 -0500

    temp save

--- src/modules/SdnDiag.Common/public/Enable-SdnVipTrace.ps1
+++ src/modules/SdnDiag.Common/public/Enable-SdnVipTrace.ps1
@@ -8,15 +8,29 @@ function Enable-SdnVipTrace {
         [System.String]$VirtualIP,
 
         [Parameter(Mandatory = $false)]
+        [ValidateScript({
+            if ($_.Scheme -ne "http" -and $_.Scheme -ne "https") {
+                throw New-Object System.FormatException("Parameter is expected to be in http:// or https:// format.")
+            }
+            return $true
+        })]
+        [Uri]$NcUri,
+
+        [Parameter(Mandatory = $false)]
         [System.Management.Automation.PSCredential]
         [System.Management.Automation.Credential()]
-        $Credential = [System.Management.Automation.PSCredential]::Empty
+        $NcRestCredential = [System.Management.Automation.PSCredential]::Empty,
+
+        [Parameter(Mandatory = $false)]
+        [System.Management.Automation.PSCredential]
+        [System.Management.Automation.Credential()]
+        $HostCredential = [System.Management.Automation.PSCredential]::Empty
     )
 
     try {
 
     }
     catch {
-        "{0}`n{1}" -f $_.Exception, $_.ScriptStackTrace | Trace-Output -Level:Err
+        "{0}`n{1}" -f $_.Exception, $_.ScriptStackTrace | Trace-Output -Level:Error
     }
 }
