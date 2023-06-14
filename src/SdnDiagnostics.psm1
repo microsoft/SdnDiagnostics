24bb78aa730b83e40e1bda1cfdd4074da35506bd
Adam Rudell <arudell@microsoft.com>
2023-06-14 09:00:59 -0500

    temp save changes

--- src/SdnDiagnostics.psm1
+++ src/SdnDiagnostics.psm1
@@ -3,6 +3,11 @@
 
 Import-Module $PSScriptRoot\SdnDiagnostics.Helper.psm1
 
+if (Get-Module -Name 'test-sdnexpressbgp' -ErrorAction SilentlyContinue) {
+    Remove-Module -Name 'test-sdnexpressbgp' -Force -ErrorAction SilentlyContinue
+}
+Import-Module "$PSScriptRoot\externalPackages\test-sdnexpressbgp.psd1" -Scope Global
+
 New-Variable -Name 'SdnDiagnostics' -Scope 'Global' -Force -Value @{
     Cache = @{}
     EnvironmentInfo = @{
