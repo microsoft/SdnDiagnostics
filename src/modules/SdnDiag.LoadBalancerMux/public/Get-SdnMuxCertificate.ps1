4e99c60ef828f8c02ff88a462544c0738430630a
Adam Rudell <arudell@microsoft.com>
2023-06-23 13:08:54 -0500

    add new functions to return server and mux cert

---
+++ src/modules/SdnDiag.LoadBalancerMux/public/Get-SdnMuxCertificate.ps1
@@ -0,0 +1,19 @@
+function Get-SdnMuxCertificate {
+    <#
+        .SYNOPSIS
+        Returns the certificate used by the SDN Load Balancer Mux.
+    #>
+
+    [CmdletBinding()]
+    param ()
+
+    try {
+        $muxCert = Get-ItemPropertyValue -Path 'HKLM:\SYSTEM\CurrentControlSet\Services\SlbMux' -Name 'MuxCert'
+        $subjectName = "CN={0}" -f $muxCert
+        $certificate = Get-SdnCertificate -Subject $subjectName
+        return $certificate
+    }
+    catch {
+        "{0}`n{1}" -f $_.Exception, $_.ScriptStackTrace | Trace-Output -Level:Error
+    }
+}
