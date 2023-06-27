45c5ca3c953101e27b0794d25bb60acad58b335a
Adam Rudell <arudell@microsoft.com>
2023-06-27 02:57:29 -0500

    Add functions to return certificate for mux and server (#187)
    * add new functions to return server and mux cert
    
    * update to include path param

---
+++ src/modules/SdnDiag.Server/public/Get-SdnServerCertificate.ps1
@@ -0,0 +1,19 @@
+function Get-SdnServerCertificate {
+    <#
+        .SYNOPSIS
+        Returns the certificate used by the SDN Host Agent.
+    #>
+
+    [CmdletBinding()]
+    param()
+
+    try {
+        $serverCert = Get-ItemPropertyValue -Path 'HKLM:\SYSTEM\CurrentControlSet\Services\NcHostAgent\Parameters' -Name 'HostAgentCertificateCName'
+        $subjectName = "CN={0}" -f $serverCert
+        $certificate = Get-SdnCertificate -Subject $subjectName -Path 'Cert:\LocalMachine\My'
+        return $certificate
+    }
+    catch {
+        "{0}`n{1}" -f $_.Exception, $_.ScriptStackTrace | Trace-Output -Level:Error
+    }
+}
