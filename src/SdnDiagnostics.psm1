ef93bd7ffa82ea91b0941928afbc50a8be818274
Adam Rudell <arudell@microsoft.com>
2023-09-27 00:12:32 -0500

    Add Test-SdnExpressBGP module into SdnDiagnostics (#206)
    * temp save changes
    
    * integrate sdnexpressbgp into module
    
    * update build process

--- src/SdnDiagnostics.psm1
+++ src/SdnDiagnostics.psm1
@@ -2,7 +2,6 @@
 # Licensed under the MIT License.
 
 Import-Module $PSScriptRoot\SdnDiagnostics.Helper.psm1
-
 New-Variable -Name 'SdnDiagnostics' -Scope 'Global' -Force -Value @{
     Cache = @{}
     EnvironmentInfo = @{
