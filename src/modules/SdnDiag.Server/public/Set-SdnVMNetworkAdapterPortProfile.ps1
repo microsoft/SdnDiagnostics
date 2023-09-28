24ab4ab016af38fb48e8365f9ee8ece57e1fbb1d
Adam Rudell <arudell@microsoft.com>
2023-09-28 05:40:18 -0500

    Update Set-SdnVMNetworkAdapterPortProfile (#207)
    * update cmdlet
    
    * update function example

--- src/modules/SdnDiag.Server/public/Set-SdnVMNetworkAdapterPortProfile.ps1
+++ src/modules/SdnDiag.Server/public/Set-SdnVMNetworkAdapterPortProfile.ps1
@@ -7,15 +7,19 @@ function Set-SdnVMNetworkAdapterPortProfile {
     .PARAMETER MacAddress
         Specifies the MAC address of the VM network adapter.
     .PARAMETER ProfileId
-        The InstanceID of the Network Interface taken from Network Controller.
+        The InstanceID of the Network Interface taken from Network Controller. If ommited, defaults to an empty GUID to enable network connectivity for non-NC managed VMs.
     .PARAMETER ProfileData
         1 = VfpEnabled, 2 = VfpDisabled (usually in the case of Mux). If ommited, defaults to 1.
+    .PARAMETER HostVmNic
+        Indicates if NIC is a host NIC. If ommited, defaults to false.
     .PARAMETER HyperVHost
         Type the NetBIOS name, an IP address, or a fully qualified domain name of the computer that is hosting the virtual machine.
     .PARAMETER Credential
         Specifies a user account that has permission to perform this action. The default is the current user.
     .EXAMPLE
-        Set-SdnVMNetworkAdapterPortProfile -VMName 'TestVM01' -MacAddress 001DD826100E
+        Set-SdnVMNetworkAdapterPortProfile -VMName 'TestVM01' -MacAddress 001DD826100E -ProfileId <InstanceIDFromNC> -ProfileData 1
+    .EXAMPLE
+        Set-SdnVMNetworkAdapterPortProfile -VMName 'TestVM01' -MacAddress 001DD826100E -ProfileData 2
     #>
 
     [CmdletBinding(DefaultParameterSetName = 'Local')]
@@ -28,13 +32,17 @@ function Set-SdnVMNetworkAdapterPortProfile {
         [Parameter(Mandatory = $true, ParameterSetName = 'Remote')]
         [System.String]$MacAddress,
 
-        [Parameter(Mandatory = $true, ParameterSetName = 'Local')]
-        [Parameter(Mandatory = $true, ParameterSetName = 'Remote')]
-        [System.Guid]$ProfileId,
+        [Parameter(Mandatory = $false, ParameterSetName = 'Local')]
+        [Parameter(Mandatory = $false, ParameterSetName = 'Remote')]
+        [System.Guid]$ProfileId = [System.Guid]::Empty,
 
         [Parameter(Mandatory = $false, ParameterSetName = 'Local')]
         [Parameter(Mandatory = $false, ParameterSetName = 'Remote')]
         [Int]$ProfileData = 1,
+
+        [Parameter(Mandatory = $false, ParameterSetName = 'Local')]
+        [Parameter(Mandatory = $false, ParameterSetName = 'Remote')]
+        [switch]$HostVmNic,
 
         [Parameter(Mandatory = $false, ParameterSetName = 'Remote')]
         [System.String]$HyperVHost,
@@ -58,7 +66,10 @@ function Set-SdnVMNetworkAdapterPortProfile {
             [System.Guid]$ProfileId,
 
             [Parameter(Mandatory = $false, Position = 3)]
-            [System.Int16]$ProfileData = 1
+            [System.Int16]$ProfileData = 1,
+
+            [Parameter(Mandatory = $false, Position = 4)]
+            [switch]$HostVmNic
         )
 
         [System.Guid]$portProfileFeatureId = "9940cd46-8b06-43bb-b9d5-93d50381fd56"
@@ -68,7 +79,13 @@ function Set-SdnVMNetworkAdapterPortProfile {
             Import-Module -Name Hyper-V -Force
         }
 
-        $vmNic = Get-VMNetworkAdapter -VMName $VmName | Where-Object {$_.MacAddress -ieq $MacAddress}
+        if ($HostVmNic) {
+            $vmNic = Get-VMNetworkAdapter -ManagementOS -VMName $VmName | Where-Object {$_.MacAddress -ieq $MacAddress}
+        }
+        else {
+            $vmNic = Get-VMNetworkAdapter -VMName $VmName | Where-Object {$_.MacAddress -ieq $MacAddress}
+        }
+
         if ($null -eq $vmNic) {
             "Unable to locate VMNetworkAdapter" | Trace-Output -Level:Exception
             return
@@ -107,15 +124,23 @@ function Set-SdnVMNetworkAdapterPortProfile {
         MacAddress = $MacAddress
         ProfileId = $ProfileId
         ProfileData = $ProfileData
+        HostVmNic = $HostVmNic
     }
 
     try {
         switch ($PSCmdlet.ParameterSetName) {
             'Remote' {
                 Invoke-PSRemoteCommand -ComputerName $HyperVHost -Credential $Credential -ScriptBlock {
-                    param([Parameter(Position = 0)][String]$param1, [Parameter(Position = 1)][String]$param2, [Parameter(Position = 2)][Guid]$param3, [Parameter(Position = 3)][Int]$param4)
+                    param(
+                        [Parameter(Position = 0)][String]$param1,
+                        [Parameter(Position = 1)][String]$param2,
+                        [Parameter(Position = 2)][Guid]$param3,
+                        [Parameter(Position = 3)][Int]$param4,
+                        [Parameter(Position = 4)][Switch]$param5
+                    )
+
                     Set-VMNetworkAdapterPortProfile -VMName $param1 -MacAddress $param2 -ProfileId $param3 -ProfileData $param4
-                } -ArgumentList @($splat.VMName, $splat.MacAddress, $splat.ProfileId, $splat.ProfileData)
+                } -ArgumentList @($splat.VMName, $splat.MacAddress, $splat.ProfileId, $splat.ProfileData, $splat.$HostVmNic)
             }
             'Local' {
                 Set-VMNetworkAdapterPortProfile @splat
