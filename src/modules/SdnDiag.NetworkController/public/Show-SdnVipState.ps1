efac99c356c94113af72daf732154b31839dfaa6
Adam Rudell <arudell@microsoft.com>
2023-10-03 14:27:35 -0500

    update catch handling

--- src/modules/SdnDiag.NetworkController/public/Show-SdnVipState.ps1
+++ src/modules/SdnDiag.NetworkController/public/Show-SdnVipState.ps1
@@ -16,6 +16,6 @@ function Show-SdnVipState {
         }
     }
     catch {
-        "{0}`n{1}" -f $_.Exception, $_.ScriptStackTrace | Trace-Output -Level:Error
+        $_ | Trace-Output -Level:Error
     }
 }
