9dad36cb7368a605c215b3aff8932e5f2f778e17
Adam Rudell <arudell@microsoft.com>
2024-01-03 09:29:46 -0600

    add new logging capabilities

---
+++ src/modules/SdnDiag.Utilities/private/Trace-Exception.ps1
@@ -0,0 +1,26 @@
+function Trace-Exception {
+    <#
+    .SYNOPSIS
+        Extracts information out of exceptions to write to the log file.
+        Pipe exceptions to this command in a catch block.
+
+    .PARAMETER Exception
+        Any exception inherited from [System.Exception]
+
+    .EXAMPLE
+        try
+        {
+            1 / 0 #divide by 0 exception
+        }
+        catch
+        {
+            $_ | Trace-Exception
+        }
+    #>
+    param(
+        [parameter(Mandatory = $True, ValueFromPipeline = $true)]
+        $Exception
+    )
+
+    Trace-Output -Level:Error -Message $Exception.Exception.Message -Exception $Exception
+}
