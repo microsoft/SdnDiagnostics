ecba701bf9a109fcd26b1f5437de2fa53d31f93c
Adam Rudell <arudell@microsoft.com>
2024-01-25 10:58:25 -0600

    add function to return ilb information

---
+++ src/modules/SdnDiag.NetworkController/public/Get-SdnInternalLoadBalancer.ps1
@@ -0,0 +1,113 @@
+function Get-SdnInternalLoadBalancer {
+    <#
+        .SYNOPSIS
+            Performs lookups and joins between OVSDB resources, load balancers and virtual networks to create internal load balancer object mappings
+        .PARAMETER NcUri
+            Specifies the Network Controller URI to connect to.
+        .PARAMETER Credential
+            Specifies a user account that has permission to the Hyper-V Hosts within the SDN Fabric. The default is the current user.
+        .PARAMETER NcRestCredential
+            Specifies a user account that has permission to query the Network Controller NB API endpoint. The default is the current user.
+        .EXAMPLE
+            Get-SdnInternalLoadBalancer -NcUri https://nc.contoso.com -Credential (Get-Credential)
+    #>
+
+    param (
+        [Parameter(Mandatory = $true)]
+        [Uri]$NcUri,
+
+        [Parameter(Mandatory = $false)]
+        [IPAddress]$IPAddress,
+
+        [Parameter(Mandatory = $false)]
+        [System.Management.Automation.PSCredential]
+        [System.Management.Automation.Credential()]
+        $Credential = [System.Management.Automation.PSCredential]::Empty,
+
+        [Parameter(Mandatory = $false)]
+        [System.Management.Automation.PSCredential]
+        [System.Management.Automation.Credential()]
+        $NcRestCredential = [System.Management.Automation.PSCredential]::Empty
+    )
+
+
+    $array = @()
+    $subnetHash = [System.Collections.Hashtable]::new()
+    $frontendHash = [System.Collections.Hashtable]::new()
+
+    try {
+        $servers = Get-SdnServer -NcUri $NcUri -Credential $NcRestCredential -ManagementAddressOnly
+        $ovsdbAddressMappings = Get-SdnOvsdbAddressMapping -ComputerName $servers -Credential $Credential | Where-Object {$_.mappingType -eq 'learning_disabled'}
+        $loadBalancers = Get-SdnResource -NcUri $NcUri -Credential $NcRestCredential -Resource LoadBalancers
+        $virtualNetworks = Get-SdnResource-NcUri $NcUri -Credential $NcRestCredential -Resource VirtualNetworks
+
+        # if this returns null, this is due to no tenant internal load balancers have been provisioned on the system
+        # in which case all the further processing is not needed
+        if($null -eq $ovsdbAddressMappings){
+            "No tenant Internal Load Balancer references found within OVSDB" | Trace-Output -Level:Verbose
+            return $null
+        }
+
+        "Located {0} address mappings from OVSDB" -f $ovsdbAddressMappings.Count | Trace-Output -Level:Verbose
+        # create a hash table based on the subnet instanceId contained within the virtual networks
+        foreach($group in $virtualNetworks.properties.subnets | Group-Object InstanceID){
+            [void]$subnetHash.Add($group.Name, $group.Group)
+        }
+
+        "Located {0} subnets" -f $subnetHash.Count | Trace-Output -Level:Verbose
+        # create a hash table based on the resourceRef of the frontendIPConfigurations within the load balancers
+        foreach($group in $loadBalancers.properties.frontendIPConfigurations | Group-Object resourceRef){
+            [void]$frontendHash.Add($group.Name, $group.Group)
+        }
+
+        "Located {0} frontendIPConfigurations" -f $frontendHash.Count | Trace-Output -Level:Verbose
+        foreach($ovsdbObject in $ovsdbAddressMappings){
+
+            # leveraging the routing domain ID taken from the OVSDB objects we need to
+            # do a lookup against the virtual network subnets to locate the associated ip configurations
+            # once we have the ipconfiguration, we want to enumerate each load balancer to match on the customer ip address
+            $tenantSubnet = $subnetHash[$ovsdbObject.RoutingDomainID.Guid]
+            if($tenantSubnet){
+                $loadBalancerResourceRef = $tenantSubnet.properties.ipConfigurations | Where-Object {$_.ResourceRef -like "/loadBalancers/*"}
+                if($loadBalancerResourceRef){
+                    foreach($resource in $loadBalancerResourceRef){
+                        $internalLoadBalancer = $frontendHash[$resource.resourceRef]
+
+                        # if the customer ip address does not match between load balancer and ovsdb then skip it as
+                        # this is not the load balancer you are looking for
+                        if($internalLoadBalancer){
+                            if($internalLoadBalancer.properties.privateIPAddress -ne $ovsdbObject.CustomerAddress){
+                                continue
+                            }
+
+                            # create a new object to add to the array list as we now have all the mappings we want
+                            $array += [PSCustomObject]@{
+                                [String]ResourceRef = $internalLoadBalancer.resourceRef
+                                [IPAddress]CustomerAddress = $internalLoadBalancer.properties.privateIPAddress
+                                [IPAddress]ProviderAddress = $ovsdbObject.ProviderAddress
+                            }
+                        }
+                        else {
+                            "Unable to locate Load Balancer Frontend IP Configuration for {0}" -f $resource.resourceRef | Trace-Output -Level:Warning
+                        }
+                    }
+                }
+                else {
+                    "Unable to locate any Load Balancer objects within IP configurations for {0}" -f $tenantSubnet.resourceRef  | Trace-Output -Level:Warning
+                }
+            }
+            else {
+                "Unable to locate Virtual Network Subnet related to Routing Domain ID {0}" -f $ovsdbObject.RoutingDomainID | Trace-Output -Level:Warning
+            }
+        }
+
+        if ($IPAddress) {
+            return ($array | Where-Object {$_.CustomerAddress -eq $IPAddress})
+        }
+
+        return ($array | Sort-Object CustomerAddress -Unique)
+    }
+    catch {
+        $_ | Trace-Exception
+    }
+}
