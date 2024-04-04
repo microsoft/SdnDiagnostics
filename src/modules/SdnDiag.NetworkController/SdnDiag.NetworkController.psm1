d100039930bcb5ec71f0f70aac0731311ef75e01
Adam Rudell <arudell@microsoft.com>
2024-04-04 11:52:50 -0500

    move NC SF cmdlets to new module

--- src/modules/SdnDiag.NetworkController/SdnDiag.NetworkController.psm1
+++ src/modules/SdnDiag.NetworkController/SdnDiag.NetworkController.psm1
@@ -3,6 +3,7 @@
 
 Using module .\SdnDiag.NetworkController.Helper.psm1
 Import-Module $PSScriptRoot\SdnDiag.NetworkController.Helper.psm1
+Import-Module $PSScriptRoot\..\SdnDiag.NetworkController.ServiceFabric\SdnDiag.NetworkController.ServiceFabric.psm1
 Import-Module $PSScriptRoot\..\SdnDiag.Common\SdnDiag.Common.psm1
 Import-Module $PSScriptRoot\..\SdnDiag.Utilities\SdnDiag.Utilities.psm1
 
