microsoft/SdnDiagnostics

commit f9398ef3fe9b8cc3485c56b252871c77f4836a9b

Improve functionality around Network Controller and Service Fabric cmdlets (#265)
# Description
This pull request primarily includes changes to the `SdnDiag` module in
the `src` directory. The changes focus on modifying the way Network
Controller information is retrieved and used. The most important changes
include modifying the Network Controller's default value to use the
environment variable `COMPUTERNAME`, updating several functions to use
the first element of the `NetworkController` array, and adding a new
parameter `Name` to some functions to specify the friendly name of the
node for the network controller.

Changes to Network Controller default value:

*
[`src/modules/SdnDiag.Health/public/Debug-SdnFabricInfrastructure.ps1`](diffhunk://#diff-324c08d6d20cb9d37d1a5b9756303f40024eb3d5c4008d8b210c8e7295ea1151L25-R25):
Modified the default value of `NetworkController` to use the
`COMPUTERNAME` environment variable instead of `HostName`.

Changes to Network Controller usage:

* Various files in `src/modules/SdnDiag.Health/private/`: Updated
several functions to use the first element of the `NetworkController`
array instead of the whole array. This includes
`Test-NcHostAgentConnectionToApiService`, `Test-NcUrlNameResolution`,
`Test-ServiceFabricApplicationHealth`,
`Test-ServiceFabricClusterHealth`, `Test-ServiceFabricNodeStatus`, and
`Test-ServiceFabricPartitionDatabaseSize`.
[[1]](diffhunk://#diff-68dbaf962eab89a5e9ffd87f730d4eb00c609721954c0572f8e470a82949546aL45-R45)
[[2]](diffhunk://#diff-53875d037e2095362d1f4d2ebce09ba56c67e194efbb31a4155b7fc6a28b3a43L24-R24)
[[3]](diffhunk://#diff-668704dd9169d77f54a7cd6d4b510ec1e6dc285a592c37ad62d909547c7b99fbL23-R28)
[[4]](diffhunk://#diff-e5e0d69262c7963f0a6c15a8219173e2212dadb005c38dde67c19939b33f2f4aL23-R28)
[[5]](diffhunk://#diff-48f1eadb283a67fe794b6b4010a03fcf8d1951cb59507670bbe5dc6c2e882fc9L23-R23)
[[6]](diffhunk://#diff-bd764ff1ecb6ac1be4d69275151ea1ef4733b8114c22aadedcd95075feec9f94L24-R34)

src/modules/SdnDiag.NetworkController/private/Get-SdnNetworkControllerInfoFromClusterManifest.ps1
@@ -4,6 +4,8 @@ function Get-SdnNetworkControllerInfoFromClusterManifest {
         Get the Network Controller Configuration from network controller cluster manifest file. The function is used to retrieve information of the network controller when cluster down.
     .PARAMETER NetworkController
         Specifies the name the network controller node on which this cmdlet operates. The parameter is optional if running on network controller node.
+    .PARAMETER Name
+        Specifies the friendly name of the node for the network controller. If not provided, settings are retrieved for all nodes in the deployment.
     .PARAMETER Credential
         Specifies a user account that has permission to perform this action. The default is the current user.
     #>
@@ -14,42 +16,37 @@ function Get-SdnNetworkControllerInfoFromClusterManifest {
         [String]$NetworkController = $(HostName),
 
         [Parameter(Mandatory = $false)]
+        [String]$Name,
+
+        [Parameter(Mandatory = $false)]
         [System.Management.Automation.PSCredential]
         [System.Management.Automation.Credential()]
         $Credential = [System.Management.Automation.PSCredential]::Empty
     )
 
-    "Attempting to retrieve NetworkController information via ClusterManifest" | Trace-Output
+    "Attempting to retrieve NetworkControllerNode information via ClusterManifest" | Trace-Output
+    $array = @()
 
-    $clusterManifestXml = [xml](Get-SdnServiceFabricClusterManifest -NetworkController $NetworkController -Credential $Credential)
-    $nodeList = $clusterManifestXml.ClusterManifest.Infrastructure.WindowsServer.NodeList.Node.NodeName
-    $secretCertThumbprint = $clusterManifestXml.ClusterManifest.Certificates.SecretsCertificate.X509FindValue
+    $clusterManifest = [xml](Get-SdnServiceFabricClusterManifest -NetworkController $NetworkController -Credential $Credential)
+    $clusterManifest.ClusterManifest.Infrastructure.WindowsServer.NodeList.Node | ForEach-Object {
+        $object = [PSCustomObject]@{
+            Name = $_.NodeName
+            Server = $_.IPAddressOrFQDN
+            FaultDomain = $_.FaultDomain
+            RestInterface = $null
+            Status = $null
+            NodeCertificate = $null
+        }
 
-    $splat = @{
-        Path = 'Cert:\LocalMachine\My'
-        Thumbprint = $secretCertThumbprint
+	    $certificate = ($clusterManifest.ClusterManifest.NodeTypes.NodeType | Where-Object Name -ieq $_.NodeName).Certificates.ServerCertificate.X509FindValue.ToString()
+        $object | Add-Member -MemberType NoteProperty -Name NodeCertificateThumbprint -Value $certificate
+
+        $array += $object
     }
 
-    if (Test-ComputerNameIsLocal -ComputerName $NetworkController) {
-        $serverCertificate = Get-SdnCertificate @splat
-    }
-    else {
-        $serverCertificate = Invoke-PSRemoteCommand -ComputerName $NetworkController -Credential $Credential -ScriptBlock {
-            param([Parameter(Position = 0)][String]$param1, [Parameter(Position = 1)][String]$param2)
-            Get-SdnCertificate -Path $param1 -Thumbprint $param2
-        } -ArgumentList @($splat.Path, $splat.Thumbprint)
+    if ($Name) {
+        return ($array | Where-Object { $_.Name.Split(".")[0] -ieq $Name.Split(".")[0] -or $_.Server -ieq $Name.Split(".")[0] })
     }
 
-    $infraInfo = [PSCustomObject]@{
-        Node = $nodeList
-        ClientAuthentication = $null
-        ClientCertificateThumbprint = $null
-        ClientSecurityGroup = $null
-        ServerCertificate = $serverCertificate
-        RestIPAddress = $null
-        RestName = $null
-        Version = $null
-    }
-
-    return $infraInfo
+    return $array
 }

src/SdnDiagnostics.psd1
@@ -96,6 +96,7 @@
         'Get-SdnResource',
         'Get-SdnServer',
         'Get-SdnServerCertificate',
+        'Get-SdnServiceFabricApplication',
         'Get-SdnServiceFabricApplicationHealth',
         'Get-SdnServiceFabricClusterConfig',
         'Get-SdnServiceFabricClusterHealth',

src/modules/SdnDiag.Health/private/Test-NcHostAgentConnectionToApiService.ps1
@@ -42,7 +42,7 @@ function Test-NcHostAgentConnectionToApiService {
         # get the current primary replica of Network Controller
         # if we cannot return the primary replica, then something is critically wrong with Network Controller
         # in which case we should mark this test as failed and return back to the caller with guidance to fix the SlbManagerService
-        $primaryReplicaNode = Get-SdnServiceFabricReplica -NetworkController $SdnEnvironmentObject.EnvironmentInfo.NetworkController -ServiceTypeName 'ApiService' -Credential $NcRestCredential -Primary
+        $primaryReplicaNode = Get-SdnServiceFabricReplica -NetworkController $SdnEnvironmentObject.EnvironmentInfo.NetworkController[0] -ServiceTypeName 'ApiService' -Credential $Credential -Primary
         if ($null -ieq $primaryReplicaNode) {
             "Unable to return primary replica of ApiService" | Trace-Output -Level:Error
             $sdnHealthObject.Result = 'FAIL'

src/modules/SdnDiag.Health/private/Test-NcUrlNameResolution.ps1
@@ -21,7 +21,7 @@ function Test-NcUrlNameResolution {
     try {
         "Validate that the Network Controller NB API URL resolves to the correct IP address" | Trace-Output
 
-        $ncApiReplicaPrimary = Get-SdnServiceFabricReplica -NetworkController $SdnEnvironmentObject.ComputerName -Credential $Credential -ServiceTypeName 'ApiService' -Primary
+        $ncApiReplicaPrimary = Get-SdnServiceFabricReplica -NetworkController $SdnEnvironmentObject.ComputerName[0] -Credential $Credential -ServiceTypeName 'ApiService' -Primary
         if ($null -eq $ncApiReplicaPrimary) {
             "Unable to find the primary replica for the ApiService" | Trace-Output -Level:Warning
             return $sdnHealthObject

src/modules/SdnDiag.Health/private/Test-ServiceFabricApplicationHealth.ps1
@@ -20,12 +20,12 @@ function Test-ServiceFabricApplicationHealth {
     try {
         "Validating the Service Fabric Application Health for Network Controller" | Trace-Output
 
-        $ncNodes = Get-SdnServiceFabricNode -NetworkController $SdnEnvironmentObject.ComputerName -Credential $credential
+        $ncNodes = Get-SdnServiceFabricNode -NetworkController $SdnEnvironmentObject.ComputerName[0] -Credential $credential
         if($null -eq $ncNodes){
             throw New-Object System.NullReferenceException("Unable to retrieve service fabric nodes")
         }
 
-        $applicationHealth = Get-SdnServiceFabricApplicationHealth -NetworkController $SdnEnvironmentObject.ComputerName -Credential $Credential
+        $applicationHealth = Get-SdnServiceFabricApplicationHealth -NetworkController $SdnEnvironmentObject.ComputerName[0] -Credential $Credential
         if ($applicationHealth.AggregatedHealthState -ine 'Ok') {
             $sdnHealthObject.Result = 'FAIL'
             $sdnHealthObject.Remediation += "Examine the Service Fabric Application Health for Network Controller to determine why the health is not OK."

src/modules/SdnDiag.Health/private/Test-ServiceFabricClusterHealth.ps1
@@ -20,12 +20,12 @@ function Test-ServiceFabricClusterHealth {
     try {
         "Validating the Service Fabric Cluster Health for Network Controller" | Trace-Output
 
-        $ncNodes = Get-SdnServiceFabricNode -NetworkController $SdnEnvironmentObject.ComputerName -Credential $credential
+        $ncNodes = Get-SdnServiceFabricNode -NetworkController $SdnEnvironmentObject.ComputerName[0] -Credential $credential
         if($null -eq $ncNodes){
             throw New-Object System.NullReferenceException("Unable to retrieve service fabric nodes")
         }
 
-        $clusterHealth = Get-SdnServiceFabricClusterHealth -NetworkController $SdnEnvironmentObject.ComputerName -Credential $Credential
+        $clusterHealth = Get-SdnServiceFabricClusterHealth -NetworkController $SdnEnvironmentObject.ComputerName[0] -Credential $Credential
         if ($clusterHealth.AggregatedHealthState -ine 'Ok') {
             $sdnHealthObject.Result = 'FAIL'
             $sdnHealthObject.Remediation += "Examine the Service Fabric Cluster Health for Network Controller to determine why the health is not OK."

src/modules/SdnDiag.Health/private/Test-ServiceFabricNodeStatus.ps1
@@ -20,7 +20,7 @@ function Test-ServiceFabricNodeStatus {
     try {
         "Validating the Service Fabric Nodes for Network Controller" | Trace-Output
 
-        $ncNodes = Get-SdnServiceFabricNode -NetworkController $SdnEnvironmentObject.ComputerName -Credential $credential
+        $ncNodes = Get-SdnServiceFabricNode -NetworkController $SdnEnvironmentObject.ComputerName[0] -Credential $credential
         if($null -eq $ncNodes){
             throw New-Object System.NullReferenceException("Unable to retrieve service fabric nodes")
         }

src/modules/SdnDiag.Health/private/Test-ServiceFabricPartitionDatabaseSize.ps1
@@ -21,14 +21,17 @@ function Test-ServiceFabricPartitionDatabaseSize {
     try {
         "Validate the size of the Service Fabric Partition Databases for Network Controller services" | Trace-Output
 
-        $ncNodes = Get-SdnServiceFabricNode -NetworkController $SdnEnvironmentObject.ComputerName -Credential $credential
+        $ncNodes = Get-SdnServiceFabricNode -NetworkController $SdnEnvironmentObject.ComputerName[0] -Credential $credential
         if($null -eq $ncNodes){
             throw New-Object System.NullReferenceException("Unable to retrieve service fabric nodes")
         }
 
         foreach($node in $ncNodes){
-            $ncApp = Invoke-SdnServiceFabricCommand -NetworkController $SdnEnvironmentObject.ComputerName -Credential $Credential -ScriptBlock {
+            $ncApp = Invoke-SdnServiceFabricCommand -NetworkController $SdnEnvironmentObject.ComputerName[0] -Credential $Credential -ScriptBlock {
                 param([Parameter(Position = 0)][String]$param1)
+
+                # The 3>$null 4>$null sends unwanted verbose and debug streams into the bit bucket
+                $null = Connect-ServiceFabricCluster -TimeoutSec 15 3>$null 4>$null
                 Get-ServiceFabricDeployedApplication -ApplicationName 'fabric:/NetworkController' -NodeName $param1
             } -ArgumentList @($node.NodeName.ToString())
 
@@ -38,10 +41,10 @@ function Test-ServiceFabricPartitionDatabaseSize {
             }
 
             # Only stateful service have the database file
-            $ncServices = Get-SdnServiceFabricService -NetworkController $SdnEnvironmentObject.ComputerName -Credential $Credential | Where-Object {$_.ServiceKind -eq "Stateful"}
+            $ncServices = Get-SdnServiceFabricService -NetworkController $SdnEnvironmentObject.ComputerName[0] -Credential $Credential | Where-Object {$_.ServiceKind -eq "Stateful"}
 
             foreach ($ncService in $ncServices){
-                $replica = Get-SdnServiceFabricReplica -NetworkController $SdnEnvironmentObject.ComputerName -ServiceName $ncService.ServiceName -Credential $Credential | Where-Object {$_.NodeName -eq $node.NodeName}
+                $replica = Get-SdnServiceFabricReplica -NetworkController $SdnEnvironmentObject.ComputerName[0] -ServiceName $ncService.ServiceName -Credential $Credential | Where-Object {$_.NodeName -eq $node.NodeName}
                 $imosStorePath = Join-Path -Path $ncAppWorkDir -ChildPath "P_$($replica.PartitionId)\R_$($replica.ReplicaId)\ImosStore"
                 $imosStoreFile = Invoke-PSRemoteCommand -ComputerName $node.NodeName -Credential $Credential -ScriptBlock {
                     param([Parameter(Position = 0)][String]$param1)

commit 397a691a3e960bd9efa36328e2b25f17d481e86e

Fix regression of NC information from clustermanifest (#283)
# Description
Summary of changes:
- Fix regression introduced in #265 where
`Get-NetworkControllerNodeInfoFromClusterManifest` is returning node
information rather than NetworkController information

# Change type
- [x] Bug fix (non-breaking change)
- [ ] Code style update (formatting, local variables)
- [ ] New Feature (non-breaking change that adds new functionality
without impacting existing)
- [ ] Breaking change (fix or feature that may cause functionality
impact)
- [ ] Other

# Checklist:
- [x] My code follows the style and contribution guidelines of this
project.
- [x] I have tested and validated my code changes.

src/modules/SdnDiag.NetworkController/private/Get-SdnNetworkControllerInfoFromClusterManifest.ps1
@@ -4,8 +4,6 @@ function Get-SdnNetworkControllerInfoFromClusterManifest {
         Get the Network Controller Configuration from network controller cluster manifest file. The function is used to retrieve information of the network controller when cluster down.
     .PARAMETER NetworkController
         Specifies the name the network controller node on which this cmdlet operates. The parameter is optional if running on network controller node.
-    .PARAMETER Name
-        Specifies the friendly name of the node for the network controller. If not provided, settings are retrieved for all nodes in the deployment.
     .PARAMETER Credential
         Specifies a user account that has permission to perform this action. The default is the current user.
     #>
@@ -13,10 +11,7 @@ function Get-SdnNetworkControllerInfoFromClusterManifest {
     [CmdletBinding()]
     param (
         [Parameter(Mandatory = $false)]
-        [String]$NetworkController = $(HostName),
-
-        [Parameter(Mandatory = $false)]
-        [String]$Name,
+        [String]$NetworkController = $env:COMPUTERNAME,
 
         [Parameter(Mandatory = $false)]
         [System.Management.Automation.PSCredential]
@@ -24,29 +19,37 @@ function Get-SdnNetworkControllerInfoFromClusterManifest {
         $Credential = [System.Management.Automation.PSCredential]::Empty
     )
 
-    "Attempting to retrieve NetworkControllerNode information via ClusterManifest" | Trace-Output
-    $array = @()
+    "Attempting to retrieve NetworkController information via ClusterManifest" | Trace-Output
 
-    $clusterManifest = [xml](Get-SdnServiceFabricClusterManifest -NetworkController $NetworkController -Credential $Credential)
-    $clusterManifest.ClusterManifest.Infrastructure.WindowsServer.NodeList.Node | ForEach-Object {
-        $object = [PSCustomObject]@{
-            Name = $_.NodeName
-            Server = $_.IPAddressOrFQDN
-            FaultDomain = $_.FaultDomain
-            RestInterface = $null
-            Status = $null
-            NodeCertificate = $null
-        }
+    $clusterManifestXml = [xml](Get-SdnServiceFabricClusterManifest -NetworkController $NetworkController -Credential $Credential)
+    $nodeList = $clusterManifestXml.ClusterManifest.Infrastructure.WindowsServer.NodeList.Node.NodeName
+    $secretCertThumbprint = $clusterManifestXml.ClusterManifest.Certificates.SecretsCertificate.X509FindValue
 
-	    $certificate = ($clusterManifest.ClusterManifest.NodeTypes.NodeType | Where-Object Name -ieq $_.NodeName).Certificates.ServerCertificate.X509FindValue.ToString()
-        $object | Add-Member -MemberType NoteProperty -Name NodeCertificateThumbprint -Value $certificate
-
-        $array += $object
+    $splat = @{
+        Path = 'Cert:\LocalMachine\My'
+        Thumbprint = $secretCertThumbprint
     }
 
-    if ($Name) {
-        return ($array | Where-Object { $_.Name.Split(".")[0] -ieq $Name.Split(".")[0] -or $_.Server -ieq $Name.Split(".")[0] })
+    if (Test-ComputerNameIsLocal -ComputerName $NetworkController) {
+        $serverCertificate = Get-SdnCertificate @splat
+    }
+    else {
+        $serverCertificate = Invoke-PSRemoteCommand -ComputerName $NetworkController -Credential $Credential -ScriptBlock {
+            param([Parameter(Position = 0)][String]$param1, [Parameter(Position = 1)][String]$param2)
+            Get-SdnCertificate -Path $param1 -Thumbprint $param2
+        } -ArgumentList @($splat.Path, $splat.Thumbprint)
     }
 
-    return $array
+    $infraInfo = [PSCustomObject]@{
+        Node = $nodeList
+        ClientAuthentication = $null
+        ClientCertificateThumbprint = $null
+        ClientSecurityGroup = $null
+        ServerCertificate = $serverCertificate
+        RestIPAddress = $null
+        RestName = $null
+        Version = $null
+    }
+
+    return $infraInfo
 }