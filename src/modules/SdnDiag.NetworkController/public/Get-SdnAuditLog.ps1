d5c329e172b31b21221912fd7a0475418df18541
Adam Rudell <arudell@microsoft.com>
2024-08-19 10:05:12 -0500

    save current changes

--- src/modules/SdnDiag.NetworkController/public/Get-SdnAuditLog.ps1
+++ src/modules/SdnDiag.NetworkController/public/Get-SdnAuditLog.ps1
@@ -1,4 +1,3 @@
-
 function Get-SdnAuditLog {
     <#
     .SYNOPSIS
@@ -37,23 +36,10 @@ function Get-SdnAuditLog {
         $Credential = [System.Management.Automation.PSCredential]::Empty
     )
 
-    # verify that the environment we are on supports at least v3 API and later
-    # as described in https://learn.microsoft.com/en-us/openspecs/windows_protocols/ms-ncnbi/dc23b547-9ec4-4cb3-ab20-a6bfe01ddafb
-    $currentRestVersion = (Get-SdnResource -NcUri $NcUri.AbsoluteUri -Resource 'Discovery' -Credential $NcRestCredential).properties.currentRestVersion
-    [int]$currentRestVersionInt = $currentRestVersion.Replace('V','').Replace('v','').Trim()
-    if ($currentRestVersionInt -lt 3) {
-        "Auditing requires API version 3 or later. Network Controller supports version {0}" -f $currentRestVersionInt | Trace-Output -Level:Warning
-        return
-    }
-
-    # check to see that auditing has been enabled
-    $auditSettingsConfig = Get-SdnResource -NcUri $NcUri.AbsoluteUri -Resource 'AuditingSettingsConfig' -ApiVersion $currentRestVersion -Credential $NcRestCredential
-    if ([string]::IsNullOrEmpty($auditSettingsConfig.properties.outputDirectory)) {
-        "Audit logging is not enabled" | Trace-Output
-        return
-    }
-    else {
-        "Audit logging location: {0}" -f $auditSettingsConfig.properties.outputDirectory | Trace-Output
+    $auditSettings = Get-SdnAuditLogSetting -NcUri $NcUri -Credential $NcRestCredential
+    if ($auditSettings.Enabled -eq $false) {
+        "Audit logs are not enabled" | Trace-Output
+        return $null
     }
 
     # if $ComputerName was not specified, then attempt to locate the servers within the SDN fabric
