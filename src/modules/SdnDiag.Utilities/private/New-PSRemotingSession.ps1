008e61661a92163eaf570160888c15be0e0d0955
Adam Rudell <arudell@microsoft.com>
2024-08-20 10:42:27 -0500

    fix inner var naming conflict

--- src/modules/SdnDiag.Utilities/private/New-PSRemotingSession.ps1
+++ src/modules/SdnDiag.Utilities/private/New-PSRemotingSession.ps1
@@ -51,12 +51,13 @@ function New-PSRemotingSession {
     process {
         $ComputerName | ForEach-Object {
             $session = $null
+            $objectName = $_
 
             # check to see if session is already opened
             # if no session already exists or Force is defined, then create a new remote session
-            if ($currentActiveSessions.ComputerName -contains $_ -and !$Force) {
-                $session = ($currentActiveSessions | Where-Object { $_.ComputerName -eq $_ })[0]
-                "Located existing powershell session {0} for {1}" -f $session.Name, $_ | Trace-Output -Level:Verbose
+            if ($currentActiveSessions.ComputerName -contains $objectName -and !$Force) {
+                $session = ($currentActiveSessions | Where-Object { $_.ComputerName -eq $objectName })[0]
+                "Located existing powershell session {0} for {1}" -f $session.Name, $objectName | Trace-Output -Level:Verbose
 
                 if ($ImportModuleOnRemoteSession) {
                     $moduleImported = Invoke-Command -Session $session -ScriptBlock $confirmRemoteModuleImported -ArgumentList @($ModuleName) -ErrorAction Stop
@@ -72,16 +73,16 @@ function New-PSRemotingSession {
 
             # determine if an IP address was passed for the destination
             # if using IP address it needs to be added to the trusted hosts
-            $isIpAddress = ($_ -as [IPAddress]) -as [Bool]
+            $isIpAddress = ($objectName -as [IPAddress]) -as [Bool]
             if ($isIpAddress) {
                 try {
                     Confirm-IsAdmin
 
-                    "{0} is an ip address" -f $_ | Trace-Output -Level:Verbose
+                    "{0} is an ip address" -f $objectName | Trace-Output -Level:Verbose
                     $trustedHosts = Get-Item -Path "WSMan:\localhost\client\TrustedHosts"
-                    if ($trustedHosts.Value -notlike "*$_*" -and $trustedHosts.Value -ne "*") {
-                        "Adding {0} to {1}" -f $_, $trustedHosts.PSPath | Trace-Output
-                        Set-Item -Path "WSMan:\localhost\client\TrustedHosts" -Value $_ -Concatenate
+                    if ($trustedHosts.Value -notlike "*$objectName*" -and $trustedHosts.Value -ne "*") {
+                        "Adding {0} to {1}" -f $objectName, $trustedHosts.PSPath | Trace-Output
+                        Set-Item -Path "WSMan:\localhost\client\TrustedHosts" -Value $objectName -Concatenate
                     }
                 }
                 catch {
@@ -93,25 +94,25 @@ function New-PSRemotingSession {
             try {
                 if ($Credential -ne [System.Management.Automation.PSCredential]::Empty) {
                     "PSRemotingSession use user-defined credential" | Trace-Output -Level:Verbose
-                    $session = New-PSSession -Name "SdnDiag-$(Get-Random)" -ComputerName $_ -Credential $Credential -SessionOption (New-PSSessionOption -Culture en-US -UICulture en-US -IdleTimeout 86400000) -ErrorAction Stop
+                    $session = New-PSSession -Name "SdnDiag-$(Get-Random)" -ComputerName $objectName -Credential $Credential -SessionOption (New-PSSessionOption -Culture en-US -UICulture en-US -IdleTimeout 86400000) -ErrorAction Stop
                 }
                 else {
                     # if the credential is not defined, we want to check if we
                     if ($PSSenderInfo -and !(Get-WSManCredSSPState)) {
-                        throw New-Object System.NotSupportedException("Unable to create PSSession to $_. This operation is not supported in a remote session without supplying -Credential.")
+                        throw New-Object System.NotSupportedException("Unable to create PSSession to $objectName. This operation is not supported in a remote session without supplying -Credential.")
                     }
 
                     # if we need to create a new remote session, need to check to ensure that if using an IP Address that credentials are specified
                     # which is a requirement from a WinRM perspective. Will throw a warning and skip session creation for this computer.
                     if ($isIpAddress -and $Credential -eq [System.Management.Automation.PSCredential]::Empty) {
-                        throw New-Object System.NotSupportedException("Unable to create PSSession to $_. The Credential parameter is required when using an IP Address.")
+                        throw New-Object System.NotSupportedException("Unable to create PSSession to $objectName. The Credential parameter is required when using an IP Address.")
                     }
 
                     "PSRemotingSession use default credential" | Trace-Output -Level:Verbose
-                    $session = New-PSSession -Name "SdnDiag-$(Get-Random)" -ComputerName $_ -SessionOption (New-PSSessionOption -Culture 'en-US' -UICulture 'en-US' -IdleTimeout 86400000) -ErrorAction Stop
+                    $session = New-PSSession -Name "SdnDiag-$(Get-Random)" -ComputerName $objectName -SessionOption (New-PSSessionOption -Culture 'en-US' -UICulture 'en-US' -IdleTimeout 86400000) -ErrorAction Stop
                 }
 
-                "Created powershell session {0} to {1}" -f $session.Name, $_ | Trace-Output -Level:Verbose
+                "Created powershell session {0} to {1}" -f $session.Name, $objectName | Trace-Output -Level:Verbose
                 if ($ImportModuleOnRemoteSession) {
                     "Importing module {0} on remote session {1}" -f $ModuleName, $session.Name | Trace-Output -Level:Verbose
                     Invoke-Command -Session $session -ScriptBlock $importRemoteModule -ArgumentList @($ModuleName, $Global:SdnDiagnostics.Config) -ErrorAction Stop
@@ -121,7 +122,7 @@ function New-PSRemotingSession {
                 $remoteSessions += $session
             }
             catch {
-                "Unable to create powershell session to {0}`n`t{1}" -f $_, $_.Exception.Message | Trace-Output -Level:Error
+                "Unable to create powershell session to {0}`n`t{1}" -f $objectName, $_.Exception.Message | Trace-Output -Level:Error
                 continue
             }
         }
