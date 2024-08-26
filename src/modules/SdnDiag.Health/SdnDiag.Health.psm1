826360ea5eda0269c1ca8aad753a476a35d838d0
Adam Rudell <arudell@microsoft.com>
2024-08-26 14:45:54 -0500

    import fc and nc module

--- src/modules/SdnDiag.Health/SdnDiag.Health.psm1
+++ src/modules/SdnDiag.Health/SdnDiag.Health.psm1
@@ -5,6 +5,8 @@ Using module .\SdnDiag.Health.Helper.psm1
 
 Import-Module $PSScriptRoot\SdnDiag.Health.Helper.psm1
 Import-Module $PSScriptRoot\..\SdnDiag.Common\SdnDiag.Common.psm1
+Import-Module $PSScriptRoot\..\SdnDiag.NetworkController.FC\SdnDiag.NetworkController.FC.psm1
+Import-Module $PSScriptRoot\..\SdnDiag.NetworkController.SF\SdnDiag.NetworkController.SF.psm1
 Import-Module $PSScriptRoot\..\SdnDiag.Utilities\SdnDiag.Utilities.psm1
 
 # create local variable to store configuration data
