f5f593bc70f33efe9b870f2b13bc80f80ab252df
Adam Rudell <arudell@microsoft.com>
2024-10-23 18:28:10 -0500

    simplify build version

--- .build/generate-version.ps1
+++ .build/generate-version.ps1
@@ -1,9 +1,7 @@
-$referenceDate = ([datetime]"2021-07-15 00:00:00Z").ToUniversalTime()
-
-$major = [int]((([DateTime]::UtcNow - $referenceDate).Days / 365) +1) # years since reference date
+$major = [int]4 # Major version number
 $minor = [DateTime]::UtcNow.Year.ToString().Substring(2,2) + [DateTime]::UtcNow.Month.ToString().PadLeft(2,"0") # 2002, 2104, etc for current month
-$patch = ([DateTime]::UtcNow - $referenceDate).Days # days since reference date
-$revision = "{0:d2}{1:d2}{2:d2}" -f ([DateTime]::UtcNow.Hour),([DateTime]::UtcNow.Minute),([DateTime]::UtcNow.Second) # creates revision based on hour, minute and second
+$patch = [DateTime]::UtcNow.Day.ToString().PadLeft(2, "0")
+$revision = "{0:d2}{1:d2}" -f ([DateTime]::UtcNow.Hour),([DateTime]::UtcNow.Minute) # creates revision based on hour, minute and second
 $buildNumber = "{0}.{1}.{2}.{3}" -f $major, $minor, $patch, $revision
 
 [Environment]::SetEnvironmentVariable("CUSTOM_VERSION", $buildNumber, "User")
