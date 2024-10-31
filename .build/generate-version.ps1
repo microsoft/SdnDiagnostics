b0e8f0fc43222f0560373f0c9b0b18d0a3ede724
Adam Rudell <arudell@microsoft.com>
2024-10-30 19:43:39 -0500

    Simplify build version (#344)
    # Description
    Summary of changes:
    - Simplify the build version
    
    # Change type
    - [ ] Bug fix (non-breaking change)
    - [ ] Code style update (formatting, local variables)
    - [ ] New Feature (non-breaking change that adds new functionality
    without impacting existing)
    - [ ] Breaking change (fix or feature that may cause functionality
    impact)
    - [x] Other
    
    # Checklist:
    - [x] My code follows the style and contribution guidelines of this
    project.
    - [x] I have tested and validated my code changes.

--- .build/generate-version.ps1
+++ .build/generate-version.ps1
@@ -1,10 +1,13 @@
-$referenceDate = ([datetime]"2021-07-15 00:00:00Z").ToUniversalTime()
-
-$major = [int]((([DateTime]::UtcNow - $referenceDate).Days / 365) +1) # years since reference date
+$major = [int]4 # Major version number
 $minor = [DateTime]::UtcNow.Year.ToString().Substring(2,2) + [DateTime]::UtcNow.Month.ToString().PadLeft(2,"0") # 2002, 2104, etc for current month
-$patch = ([DateTime]::UtcNow - $referenceDate).Days # days since reference date
-$revision = "{0:d2}{1:d2}{2:d2}" -f ([DateTime]::UtcNow.Hour),([DateTime]::UtcNow.Minute),([DateTime]::UtcNow.Second) # creates revision based on hour, minute and second
-$buildNumber = "{0}.{1}.{2}.{3}" -f $major, $minor, $patch, $revision
+$patch = [DateTime]::UtcNow.Day.ToString().PadLeft(2, "0")
+
+$hour = [DateTime]::UtcNow.Hour.ToString().TrimStart('0')
+$minute = [DateTime]::UtcNow.Minute.ToString().TrimStart('0')
+if ([string]::IsNullOrEmpty($minute)) { $minute = "1" }
+
+$revision = "{0}{1}" -f $hour, $minute # creates a 4 digit number from the current hour and minute
+$buildNumber = "{0}.{1}.{2}.{3}" -f $major, $minor, $patch, $revision.Trim()
 
 [Environment]::SetEnvironmentVariable("CUSTOM_VERSION", $buildNumber, "User")
 Write-Host "##vso[task.setvariable variable=CUSTOM_VERSION;]${buildNumber}"
