e7928b7c1ec5129edd689cb99e8641f2f7f6f213
Adam Rudell <arudell@microsoft.com>
2025-03-05 11:17:27 -0600

    update version generation to prevent leading 0

--- .build/generate-version.ps1
+++ .build/generate-version.ps1
@@ -1,13 +1,22 @@
 $major = [int]4 # Major version number
 $minor = [DateTime]::UtcNow.Year.ToString().Substring(2,2) + [DateTime]::UtcNow.Month.ToString().PadLeft(2,"0") # 2002, 2104, etc for current month
-$patch = [DateTime]::UtcNow.Day.ToString().PadLeft(2, "0")
 
-$hour = [DateTime]::UtcNow.Hour.ToString().TrimStart('0')
-$minute = [DateTime]::UtcNow.Minute.ToString().TrimStart('0')
-if ([string]::IsNullOrEmpty($minute)) { $minute = "1" }
+# we want to generate the patch number based on the current day
+# this will return int32 value so should be between 1 and 31
+$patch = [DateTime]::UtcNow.Day
 
-$revision = "{0}{1}" -f $hour, $minute # creates a 4 digit number from the current hour and minute
-$buildNumber = "{0}.{1}.{2}.{3}" -f $major, $minor, $patch, $revision.Trim()
+# we want to generate the revision number based on the current hour and minute
+# this can be between 2-4 digit number depending on the hour and minute
+# we want to make sure that the hour is not 0 and does not have a leading zero
+[int]$hour = [DateTime]::UtcNow.Hour
+if ($hour -ieq 0) {$hour++}
+[int]$minute = [DateTime]::UtcNow.Minute.ToString().PadLeft(2,"0")
+if ($minute -ieq 0) {$minute++}
+$revision = "{0}{1}" -f $hour.ToString().Trim(), $minute.ToString().Trim()
+
+# we now have our build number
+# we want to format as #.YYMM.DD.HHMM : 8.2104.12.1120
+$buildNumber = "{0}.{1}.{2}.{3}" -f $major, $minor, $patch.ToString().Trim(), $revision.Trim()
 
 [Environment]::SetEnvironmentVariable("CUSTOM_VERSION", $buildNumber, "User")
 Write-Host "##vso[task.setvariable variable=CUSTOM_VERSION;]${buildNumber}"
