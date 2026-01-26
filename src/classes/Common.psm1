ab51896f16e16ff41f7d87493a8959f61061ce25
Adam Rudell <arudell@microsoft.com>
2026-01-26 13:18:21 -0600

    update class object

--- src/classes/Common.psm1
+++ src/classes/Common.psm1
@@ -9,9 +9,12 @@ class SdnFabricInfrastructure {
     [System.String[]]$NetworkController
     [System.String[]]$LoadBalancerMux
     [System.String[]]$Gateway
+    [System.String[]]$Server
     [System.String]$NcUrl
     [System.String]$RestApiVersion
     [System.String[]]$FabricNodes
+    [hashtable]$FailoverClusterConfig
+    [System.String]$ClusterConfigType
 }
 
 # Export the class
